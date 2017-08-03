fe4e6113d47b5d8f578bdf3983f74644027bf4d6
Anatoliy <tolikybm@gmail.com>
2017-08-04 00:27:38 +0300

    проверка git

--- common/config/db.example.php
+++ common/config/db.example.php
@@ -1,7 +1,7 @@
 <?php
 return [
   'class' => 'yii\db\Connection',
-  'dsn' => 'mysql:host=localhost;dbname=stay2',
+  'dsn' => 'mysql:host=localhost;dbname=stay2qqqqqqqqq',
   'username' => 'root',
   'password' => '',
   'charset' => 'utf8',
