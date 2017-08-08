4318ed7aeaedc5f763634f630d0cf9e345df083a
Anatoliy <tolikybm@gmail.com>
2017-08-08 21:12:58 +0300

    Вывод таблицы

--- frontend/modules/constants/controllers/AdminController.php
+++ frontend/modules/constants/controllers/AdminController.php
@@ -55,7 +55,7 @@ class AdminController extends Controller
 
     return $this->render('index.twig', [
       'searchModel' => $searchModel,
-      'models' => $dataProvider->getModels(),
+      'dataProvider' => $dataProvider,
     ]);
   }
 
