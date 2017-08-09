4df1d762733add8a96d1c95e6081af8d9202fd9a
Andrey <oxygenn@list.ru>
2017-08-09 22:51:35 +0300

    Module Coupons. Models

---
+++ frontend/modules/coupons/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\modules\coupons;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'frontend\modules\coupons\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
