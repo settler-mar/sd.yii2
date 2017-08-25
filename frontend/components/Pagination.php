11f42c508aae1002e61739723999061b702f67c7
Andrey <oxygenn@list.ru>
2017-08-25 11:01:44 +0300

    Пагинация - сделал ошибку 404, если $page больше количества страниц

--- frontend/components/Pagination.php
+++ frontend/components/Pagination.php
@@ -9,132 +9,138 @@ use yii\helpers\Url;
 class Pagination
 {
 
-    private $pagination;
+  private $pagination;
 
-    private $activeRecord;
+  private $activeRecord;
 
-    private $options = [];
-        //'page' =>
-        //'limit' =>
-        //'asArray' => true
+  private $options = [];
+  //'page' =>
+  //'limit' =>
+  //'asArray' => true
 
-    private $cacheName;
+  private $cacheName;
 
-    /**
-     * Pagination constructor.
-     * @param $model
-     * @param array $options
-     * query
-     * 
-     */
-    public function __construct($activeRecord, $cacheName, $options = [])
-    {
-        $this->activeRecord = $activeRecord;
-        $this->options = $options;
-        $this->cacheName = $cacheName;
+  /**
+   * Pagination constructor.
+   * @param $model
+   * @param array $options
+   * query
+   *
+   */
+  public function __construct($activeRecord, $cacheName, $options = [])
+  {
+    $this->activeRecord = $activeRecord;
+    $this->options = $options;
+    $this->cacheName = $cacheName;
 
-        $cache = \Yii::$app->cache;
-        $count = $cache->getOrSet($cacheName . '_count', function () {
-            return $this->activeRecord->count();
-        });
+    $cache = \Yii::$app->cache;
+    $count = $cache->getOrSet($cacheName . '_count', function () {
+      return $this->activeRecord->count();
+    });
 
-        $this->pagination = new YiiPagination([
-            'totalCount' => $count,
-            'page' => $options['page'] - 1,
-            'pageSize' => $options['limit'],
-        ]);
+    $page = !empty($options['page']) ? $options['page'] - 1 : 0;
+    $this->pagination = new YiiPagination([
+      'totalCount' => $count,
+      'page' => $page,
+      'pageSize' => $options['limit'],
+    ]);
+    if ($page > 0 && $page > $this->pagination->pageCount - 1) {
+      throw new yii\web\NotFoundHttpException();
     }
 
-    public function data()
-    {
-        $cache = Yii::$app->cache;
-        $data = $cache->getOrSet($this->cacheName, function () {
-            $data =  $this->activeRecord
-                ->limit($this->options['limit'])
-                ->offset($this->pagination->offset);
-            if (!empty($this->options['asArray'])) {
-                $data = $data->asArray();
-            }
-            return $data->all();
-        });
-        return $data;
-    }
+  }
+
+  public function data()
+  {
+    $cache = Yii::$app->cache;
+    $data = $cache->getOrSet($this->cacheName, function () {
+      $data = $this->activeRecord
+        ->limit($this->options['limit'])
+        ->offset($this->pagination->offset);
+      if (!empty($this->options['asArray'])) {
+        $data = $data->asArray();
+      }
+      return $data->all();
+    });
+    return $data;
+  }
 
 
-    public function count()
-    {
-        return $this->pagination->totalCount;
-    }
+  public function count()
+  {
+    return $this->pagination->totalCount;
+  }
 
-    public function offset()
-    {
-        return $this->pagination->offset;
-    }
-    public function pages()
-    {
-        return $this->pagination->pageCount;
-    }
+  public function offset()
+  {
+    return $this->pagination->offset;
+  }
+
+  public function pages()
+  {
+    return $this->pagination->pageCount;
+  }
 
 
-    /**
-     * Getting ready pagination
-     * @param string $pageName
-     * @return string
-     */
-    public function getPagination($pageName, $params)
-    {
-        $displayCount = 5;//сколько кнопок отоображается в центре
-        $page = $this->pagination->page+1;
-        $total = $this->pagination->pageCount;
-        $pageName = preg_replace('/\/page-[0-9]*/', '', $pageName);
-        $pageName = preg_replace('/\/category:[0-9]*/', '', $pageName);
-        $pageName = preg_replace('/\/store:[0-9]*/', '', $pageName);
-        $params['page'] = null;
-        $pageName = array_merge(['/' . $pageName], $params);
-        //d($pageName, array_merge($params, ['page' => 5]), Url::toRoute(array_merge($params, ['page' => 5]))) ;
+  /**
+   * Getting ready pagination
+   * @param string $pageName
+   * @return string
+   */
+  public function getPagination($pageName, $params)
+  {
+    $displayCount = 5;//сколько кнопок отоображается в центре
+    $page = $this->pagination->page + 1;
+    $total = $this->pagination->pageCount;
+    $pageName = preg_replace('/\/page-[0-9]*/', '', $pageName);
+    $pageName = preg_replace('/\/category:[0-9]*/', '', $pageName);
+    $pageName = preg_replace('/\/store:[0-9]*/', '', $pageName);
+    $params['page'] = null;
+    $pageName = array_merge(['/' . $pageName], $params);
+    //d($pageName, array_merge($params, ['page' => 5]), Url::toRoute(array_merge($params, ['page' => 5]))) ;
 
-        //предыдущая
-        $prevpage = $page != 1 ? '<li class="back"><a data-toggle="tooltip" data-placement="top"' .
-            ' data-original-title="Предыдущая" href="' .
-            Url::toRoute(array_merge($pageName, ['page' => $page -1])) . '">' .
-            '<span class="fa fa fa-caret-left"></span></a></li>' : '';
+    //предыдущая
+    $prevpage = $page != 1 ? '<li class="back"><a data-toggle="tooltip" data-placement="top"' .
+      ' data-original-title="Предыдущая" href="' .
+      Url::toRoute(array_merge($pageName, ['page' => $page - 1])) . '">' .
+      '<span class="fa fa fa-caret-left"></span></a></li>' : '';
 
-        //первая
-        $first = $page >= $displayCount && $total > $displayCount ?
-            '<li class="first"><a data-toggle="tooltip" data-placement="top"' .
-            ' data-original-title="Первая" href="' .
-            Url::toRoute(array_merge($pageName, ['page' => 1])) . '">1' .
-            '</a></li>' : '';
-        //последняя
-        $last = $total - $page >= $displayCount ? '<li class="last"><a data-toggle="tooltip" data-placement="top"' .
-            ' data-original-title="Последняя" href="' .
-            Url::toRoute(array_merge($pageName, ['page' => $total])) . '">' . $total .
-            '</a></li>' : '';
+    //первая
+    $first = $page >= $displayCount && $total > $displayCount ?
+      '<li class="first"><a data-toggle="tooltip" data-placement="top"' .
+      ' data-original-title="Первая" href="' .
+      Url::toRoute(array_merge($pageName, ['page' => 1])) . '">1' .
+      '</a></li>' : '';
+    //последняя
+    $last = $total - $page >= $displayCount ? '<li class="last"><a data-toggle="tooltip" data-placement="top"' .
+      ' data-original-title="Последняя" href="' .
+      Url::toRoute(array_merge($pageName, ['page' => $total])) . '">' . $total .
+      '</a></li>' : '';
 
-        //следующая
-        $nextpage = $page != $total ? '<li class="next"><a data-toggle="tooltip" data-placement="top"' .
-            ' data-original-title="Следующая" href="' .
-            Url::toRoute(array_merge($pageName, ['page' => $page + 1])) . '">' .
-            '<span class="fa fa fa-caret-right"></span></a>' : '';
+    //следующая
+    $nextpage = $page != $total ? '<li class="next"><a data-toggle="tooltip" data-placement="top"' .
+      ' data-original-title="Следующая" href="' .
+      Url::toRoute(array_merge($pageName, ['page' => $page + 1])) . '">' .
+      '<span class="fa fa fa-caret-right"></span></a>' : '';
 
-        $pages = ($page >= $displayCount && $total > $displayCount ? '...' : '');
-        $pageStart = floor($page / ($displayCount - 1)) * ($displayCount - 1) == $page ?
-            (floor($page / ($displayCount - 1)) - 1) * ($displayCount - 1) + 1 :
-            floor($page / ($displayCount - 1)) * ($displayCount - 1) + 1;
-        $pageStart = $page < $displayCount ? 1 : ($page < $total - $displayCount + 1 ? $pageStart : $total - $displayCount + 1);
-        $pageEnd = $pageStart + $displayCount - 1;
-        $pageEnd = ($pageEnd > $total ? $total : $pageEnd);
+    $pages = ($page >= $displayCount && $total > $displayCount ? '...' : '');
+    $pageStart = floor($page / ($displayCount - 1)) * ($displayCount - 1) == $page ?
+      (floor($page / ($displayCount - 1)) - 1) * ($displayCount - 1) + 1 :
+      floor($page / ($displayCount - 1)) * ($displayCount - 1) + 1;
+    $pageStart = $page < $displayCount ? 1 : ($page < $total - $displayCount + 1 ? $pageStart : $total - $displayCount + 1);
+    $pageEnd = $pageStart + $displayCount - 1;
+    $pageEnd = ($pageEnd > $total ? $total : $pageEnd);
 
-        for ($i = $pageStart; $i <= $pageEnd; $i++) {
-            $pages .= ($i == $page ? '<li class="active">' . $i . '</li>' :
-                '<li><a href="' .
-                Url::toRoute(array_merge($pageName, ['page' => $i])) . '">' . $i . '</a></li>');
-        };
+    for ($i = $pageStart; $i <= $pageEnd; $i++) {
+      $pages .= ($i == $page ? '<li class="active">' . $i . '</li>' :
+        '<li><a href="' .
+        Url::toRoute(array_merge($pageName, ['page' => $i])) . '">' . $i . '</a></li>');
+    };
 
-        $pages .= ($total - $page <= $displayCount - 1 ? '' : '...');
+    $pages .= ($total - $page <= $displayCount - 1 ? '' : '...');
 
-        return '<ul>' . $prevpage . $first . $pages . $last . $nextpage . '</ul>';
-    }
+    return '<ul>' . $prevpage . $first . $pages . $last . $nextpage . '</ul>';
+  }
 
 
 }
