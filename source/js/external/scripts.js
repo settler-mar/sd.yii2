2cf28456e0c5718f80614e881a03fdcf8d7e7942
Andrey <oxygenn@list.ru>
2017-09-04 12:01:56 +0300

    Исправлен якорь. Поправлена страница 404

--- source/js/external/scripts.js
+++ source/js/external/scripts.js
@@ -120,7 +120,9 @@ $(document).ready(function() {
     /***************** Smooth Scrolling ******************/
     $(function() {
         $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var href = $(this).attr('href').replace(/#.*$/, '');
+            if (href === '') {
+//            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
