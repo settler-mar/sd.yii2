2bdb3bbc073581d6a5a0a9a0eca1408316f7ce3c
Andrey <oxygenn@list.ru>
2017-09-05 22:37:45 +0300

    Использование купона без регистрации - из всплывающего окна

--- source/js/original/my.js
+++ source/js/original/my.js
@@ -1,5 +1,9 @@
 $('body').on('click','a[href=#login],a[href=#registration],a[href=#resetpassword]',function(e){
   e.preventDefault();
+  var popup = $("a[href='#showpromocode-noregister']").data('popup');
+  if (popup) {
+    popup.close();
+  }
   href=this.href.split('#');
   href=href[href.length-1];
 
@@ -37,3 +41,23 @@ $(function() {
 });
 
 ajaxForm($('.ajax_form'));
+
+
+$("a[href='#showpromocode-noregister']").popup({
+  content : '<div class="coupon-noregister">'+
+  '<div class="coupon-noregister__icon"><img src="/images/templates/swa.png" alt=""></div>'+
+  '<div class="coupon-noregister__text">Для получения кэшбэка необходимо</br>авторизоваться на сайте</div>' +
+  '<div class="coupon-noregister__buttons">'+
+  '<a href="goto/coupon:{id}" target="_blank" class="btn  btn-popup">Воспользоваться</br>купоном</br>без регистрации</a>'+
+  '<a href="#registration" class="btn btn-popup">Зарегистрироваться</br>и получить</br>ещё и кэшбэк</a>'+
+  '</div>'+
+  '<div>',
+  type : 'html',
+  beforeOpen: function() {
+    var id = $(this.ele).data('id');
+    this.o.content = this.o.content.replace('{id}', id);
+  },
+  afterOpen: function() {
+    $('.popup_content')[0].innerHTML = this.o.content;
+  }
+});
