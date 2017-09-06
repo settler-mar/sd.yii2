553abad631b6171a88ff05b94d3fde0dad5e1b19
Andrey <oxygenn@list.ru>
2017-09-06 10:41:52 +0300

    Изменил Js. Popup окна - закрывают друг дгуга

--- source/js/original/my.js
+++ source/js/original/my.js
@@ -1,8 +1,14 @@
 $('body').on('click','a[href=#login],a[href=#registration],a[href=#resetpassword]',function(e){
   e.preventDefault();
+  //при открытии формы регистрации закрыть, если отрыто - попап использования купона без регистрации
   var popup = $("a[href='#showpromocode-noregister']").data('popup');
   if (popup) {
     popup.close();
+  } else {
+    popup = $('div.popup_cont, div.popup_back');
+    if (popup) {
+      popup.hide();
+    }
   }
   href=this.href.split('#');
   href=href[href.length-1];
@@ -54,8 +60,14 @@ $("a[href='#showpromocode-noregister']").popup({
   '<div>',
   type : 'html',
   beforeOpen: function() {
+    //заменить в контенте {id}
     var id = $(this.ele).data('id');
     this.o.content = this.o.content.replace('{id}', id);
+    //если закрыли принудительно, то показать
+    popup = $('div.popup_cont, div.popup_back');
+    if (popup) {
+      popup.show();
+    }
   },
   afterOpen: function() {
     $('.popup_content')[0].innerHTML = this.o.content;
