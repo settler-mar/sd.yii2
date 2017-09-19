59aa7f719d6a06afeb434320677b2a59b7af29ba
Max Matiukhin <matuhinmax@mail.ru>
2017-09-19 18:47:06 +0300

    Добавил всплывающие уведомления. Поправил layout в метотегах.

--- source/js/original/notification.js
+++ source/js/original/notification.js
@@ -1,4 +1,10 @@
 var notification = (function() {
+  var conteiner;
+  var mouseOver = 0;
+  var timerClearAll = null;
+  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+  var time = 3000;
+
   var notification_box =false;
   var is_init=false;
   var confirm_opt={
@@ -10,7 +16,6 @@ var notification = (function() {
     callbackNo:false,
     obj:false,
   };
-
   var alert_opt={
     title:"",
     question:"Сообщение",
@@ -36,12 +41,56 @@ var notification = (function() {
   function closeModal(){
     $('html').removeClass('show_notifi');
   }
+
   function closeModalFon(e){
     var target = e.target || e.srcElement;
     if(target.className=="notification_box"){
       closeModal();
     }
   }
+
+  var _setUpListeners = function() {
+    $('body').on('click', '.notification_close', _closePopup);
+    $('body').on('mouseenter', '.notification_container', _onEnter);
+    $('body').on('mouseleave', '.notification_container', _onLeave);
+  };
+
+  var _onEnter = function(event) {
+    if(event)event.preventDefault();
+    if (timerClearAll!=null) {
+      clearTimeout(timerClearAll);
+      timerClearAll = null;
+    }
+    conteiner.find('.notification_item').each(function(i){
+      var option=$(this).data('option');
+      if(option.timer) {
+        clearTimeout(option.timer);
+      }
+    });
+    mouseOver = 1;
+  };
+
+  var _onLeave = function() {
+    conteiner.find('.notification_item').each(function(i){
+      $this=$(this);
+      var option=$this.data('option');
+      if(option.time>0) {
+        option.timer = setTimeout(_closePopup.bind(option.close), option.time - 1500 + 100 * i);
+        $this.data('option',option)
+      }
+    });
+    mouseOver = 0;
+  };
+
+  var _closePopup = function(event) {
+    if(event)event.preventDefault();
+
+    var $this = $(this).parent();
+    $this.on(animationEnd, function() {
+      $(this).remove();
+    });
+    $this.addClass('notification_hide')
+  };
 
   function alert(data){
     if(!data)data={};
@@ -117,9 +166,67 @@ var notification = (function() {
 
   }
 
+  function notifi(data) {
+    if(!data)data={};
+    var option = {time : (data.time||data.time===0)?data.time:time};
+    if (!conteiner) {
+      conteiner = $('<ul/>', {
+        'class': 'notification_container'
+      });
+
+      $('body').append(conteiner);
+      _setUpListeners();
+    }
+
+    var li = $('<li/>', {
+      class: 'notification_item'
+    });
+
+    if (data.type){
+      li.addClass('notification_item-' + data.type);
+    }
+
+    var close=$('<span/>',{
+      class:'notification_close'
+    });
+    option.close=close;
+    li.append(close);
+
+    if(data.title && data.title.length>0) {
+      var title = $('<p/>', {
+        class: "notification_title"
+      });
+      title.html(data.title);
+      li.append(title);
+    }
+
+    if(data.img && data.img.length>0) {
+      var img = $('<div/>', {
+        class: "notification_img"
+      });
+      img.css('background-image','url('+data.img+')');
+      li.append(img);
+    }
+
+    var content = $('<div/>',{
+      class:"notification_content"
+    });
+    content.html(data.message);
+
+    li.append(content);
+
+    conteiner.append(li);
+
+    if(option.time>0){
+      option.timer=setTimeout(_closePopup.bind(close), option.time);
+    }
+    li.data('option',option)
+  }
+
   return {
     alert: alert,
-    confirm: confirm
+    confirm: confirm,
+    notifi: notifi,
   };
 
 })();
