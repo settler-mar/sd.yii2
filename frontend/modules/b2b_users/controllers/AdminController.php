a0574a47a642ed2338e8c6c5fde1ee0df46b364d
Max Matiukhin <matuhinmax@mail.ru>
2017-09-21 19:57:32 +0300

    Начал делать добавление шопа к b2b пользователю

--- frontend/modules/b2b_users/controllers/AdminController.php
+++ frontend/modules/b2b_users/controllers/AdminController.php
@@ -15,17 +15,17 @@ use yii\filters\VerbFilter;
  */
 class AdminController extends Controller
 {
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['post'],
-                ],
-            ],
-        ];
-    }
+  public function behaviors()
+  {
+    return [
+      'verbs' => [
+        'class' => VerbFilter::className(),
+        'actions' => [
+          'delete' => ['post'],
+        ],
+      ],
+    ];
+  }
 
   public function beforeAction($action)
   {
@@ -36,84 +36,97 @@ class AdminController extends Controller
     $this->layout = '@app/views/layouts/admin.twig';
     return true;
   }
-    /**
-     * Lists all B2bUsers models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new B2bUsersSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-        return $this->render('index.twig', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
+  /**
+   * Lists all B2bUsers models.
+   * @return mixed
+   */
+  public function actionIndex()
+  {
+    $searchModel = new B2bUsersSearch();
+    $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+    return $this->render('index.twig', [
+      'searchModel' => $searchModel,
+      'dataProvider' => $dataProvider,
+    ]);
+  }
+
+  /**
+   * Creates a new B2bUsers model.
+   * If creation is successful, the browser will be redirected to the 'view' page.
+   * @return mixed
+   */
+  public function actionCreate()
+  {
+    $model = new B2bNewUsers();
+
+    if ($model->load(Yii::$app->request->post()) && $model->save()) {
+      return $this->redirect(['update', 'id' => $model->id]);
+    } else {
+      return $this->render('create.twig', [
+        'model' => $model,
+      ]);
     }
+  }
 
-    /**
-     * Creates a new B2bUsers model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new B2bNewUsers();
+  /**
+   * Updates an existing B2bUsers model.
+   * If update is successful, the browser will be redirected to the 'view' page.
+   * @param integer $id
+   * @return mixed
+   */
+  public function actionUpdate($id)
+  {
+    $model = $this->findModel($id);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['update', 'id' => $model->id]);
-        } else {
-            return $this->render('create.twig', [
-                'model' => $model,
-            ]);
-        }
+    if ($model->load(Yii::$app->request->post()) && $model->save()) {
+      return $this->redirect(['index']);
+    } else {
+      return $this->render('update.twig', [
+        'model' => $model,
+        'user_id' => $id,
+      ]);
     }
+  }
 
-    /**
-     * Updates an existing B2bUsers model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
+  public function actionAddStore($id)
+  {
+    $out = [
+      'html' => $this->renderAjax('add_shop_step1.twig', [
+        'user_id' => $id,
+      ]),
+    ];
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['index']);
-        } else {
-            return $this->render('update.twig', [
-                'model' => $model,
-            ]);
-        }
+    return json_encode($out);
+  }
+
+  /**
+   * Deletes an existing B2bUsers model.
+   * If deletion is successful, the browser will be redirected to the 'index' page.
+   * @param integer $id
+   * @return mixed
+   */
+  public function actionDelete($id)
+  {
+    $this->findModel($id)->delete();
+
+    return $this->redirect(['index']);
+  }
+
+  /**
+   * Finds the B2bUsers model based on its primary key value.
+   * If the model is not found, a 404 HTTP exception will be thrown.
+   * @param integer $id
+   * @return B2bUsers the loaded model
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  protected function findModel($id)
+  {
+    if (($model = B2bUsers::findOne($id)) !== null) {
+      return $model;
+    } else {
+      throw new NotFoundHttpException('The requested page does not exist.');
     }
-
-    /**
-     * Deletes an existing B2bUsers model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the B2bUsers model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return B2bUsers the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = B2bUsers::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
+  }
 }
