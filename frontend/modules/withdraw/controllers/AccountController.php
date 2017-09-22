9642aea4a7c2d52eef712dea3bcd9566ec182be8
Andrey <oxygenn@list.ru>
2017-09-22 11:54:36 +0300

    Заблокирована возможность вывода денежных средств чаще одного раза в сутки

--- frontend/modules/withdraw/controllers/AccountController.php
+++ frontend/modules/withdraw/controllers/AccountController.php
@@ -62,6 +62,11 @@ class AccountController extends \yii\web\Controller
           'error' => ['Максимальная сумма для вывода '.number_format($balans['current'],2,'.',' ').'р.']
         ]);
       }
+      if ($balans['withdraw_count'] > 0){
+        return json_encode([
+          'error' => ['Можно сделать не больше одного запроса в сутки на вывод средств.']
+        ]);
+      }
 
       $withdraw = new UsersWithdraw();
       $withdraw->admin_comment='';
