94eafafe39e774edfbeca0e93c39736c706bf4fd
Max Matiukhin <matuhinmax@mail.ru>
2017-09-26 17:38:09 +0300

    Модуль API. b2b выгрузка категория для шопа

--- b2b/modules/api/controllers/DefaultController.php
+++ b2b/modules/api/controllers/DefaultController.php
@@ -3,6 +3,7 @@
 namespace app\modules\api\controllers;
 
 use b2b\modules\stores_points\models\B2bStoresPoints;
+use frontend\modules\stores\models\Stores;
 use yii\web\Controller;
 use Yii;
 
@@ -19,9 +20,11 @@ class DefaultController extends Controller
 
   public function actionLogin()
   {
+
     $request=Yii::$app->request;
     if(!$request->isPost || !$request->post('regNumber')){
       throw new \yii\web\ForbiddenHttpException('Просмотр данной страницы запрещен.');
+      http_response_code(403);
       return false;
     }
 
@@ -29,13 +32,35 @@ class DefaultController extends Controller
       ->where(['access_code'=>$request->post('regNumber')])
       ->one();
     if(!$point){
-      echo 'Не найдена торговая точка с этим номером.';
-      http_response_code(404);
-      return;
+      http_response_code(200);
+      return 'Не найдена торговая точка с этим номером.';
     }
 
     Yii::$app->session->set('point',$point->id);
     Yii::$app->session->set('store',$point->store_id);
-    return true;
+    return 'OK';
+  }
+
+  public function actionCategories()
+  {
+    $store_id=Yii::$app->session->get('store');
+
+    if(!$store_id && !$this->actionLogin()){
+      return 'Ошибка прав доступа';
+    }
+
+    $store = Stores::find()
+      ->where(['uid'=>$store_id])
+      ->one();
+
+
+    $cpa = $store->getCpaLink()->one();
+
+    $cat_ist=$cpa->getStoreActions()->asArray()->all();
+    $out=array();
+    foreach ($cat_ist as $item){
+      $out[]= '"'.$item['uid'].': '.str_replace('"',"\\\"",$item['name']).'"';
+    }
+    return implode(',',$out);
   }
 }
