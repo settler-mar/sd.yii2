3efb83390ec48a8c03c0cd545e72ffba56f415ed
Max Matiukhin <matuhinmax@mail.ru>
2017-09-27 09:51:50 +0300

    Поменял ссылку в футере

--- helpers/twigFunctionList.php
+++ helpers/twigFunctionList.php
@@ -334,7 +334,7 @@ $functionsList=[
     foreach ($flashes as $type => $flashe) {
       Yii::$app->session->removeFlash($type);
       foreach ($flashe as $txt) {
-        if($txt=='Просмотр данной страницы запрещен.'){
+        if($txt=='Просмотр данной страницы запрещен.' && Yii::$app->user->isGuest){
           $txt='Для доступа к личному кабинету вам необходимо <a href="#login">авторизоваться</a> на сайте.';
         }
         $js .= 'notification.notifi({message:\'' . $txt . '\',type:\'' . $type . '\'});' . "\n";
