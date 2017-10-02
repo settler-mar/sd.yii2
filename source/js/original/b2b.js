197302f87bfca22fe6b2b1e37a1132f8b3d66b40
Andrey <oxygenn@list.ru>
2017-10-02 22:25:15 +0300

    Убран чекбокс Выходной день. Валидация

--- source/js/original/b2b.js
+++ source/js/original/b2b.js
@@ -10,9 +10,9 @@ var storesPointCheckboxes = $('.b2b-stores-points-form input[type="checkbox"]');
 storesPointCheckboxes.click(function() {
     var name = $(this).attr('name');
     var row = $(this).closest('tr');
-    if (name.match(/B2bStoresPoints\[work_time_details\]\[\d*\]\[holiday\]/)) {
-        checkDisabled(row, this.checked, 'depends-holiday');
-    }
+    // if (name.match(/B2bStoresPoints\[work_time_details\]\[\d*\]\[holiday\]/)) {
+    //     checkDisabled(row, this.checked, 'depends-holiday');
+    // }
     if (name.match(/B2bStoresPoints\[work_time_details\]\[\d*\]\[24-hour\]/)) {
         checkDisabled(row, this.checked, 'depends-24-hour');
     }
@@ -22,19 +22,19 @@ storesPointCheckboxes.click(function() {
 $.each(storesPointCheckboxes, function(index, item){
     var name = $(item).attr('name');
     var row = $(item).closest('tr');
-    if (name.match(/B2bStoresPoints\[work_time_details\]\[\d*\]\[holiday\]/) && item.checked) {
-        checkDisabled(row, true, 'depends-holiday');
-    }
+    // if (name.match(/B2bStoresPoints\[work_time_details\]\[\d*\]\[holiday\]/) && item.checked) {
+    //     checkDisabled(row, true, 'depends-holiday');
+    // }
     if (name.match(/B2bStoresPoints\[work_time_details\]\[\d*\]\[24-hour\]/) && item.checked) {
         checkDisabled(row, true, 'depends-24-hour');
     }
 });
 
 function  checkDisabled(row, checked, className) {
-    var inputsCheckbox = $(row).find('input[type="checkbox"].'+className);
+    //var inputsCheckbox = $(row).find('input[type="checkbox"].'+className);
     var inputsText = $(row).find('input[type="text"].'+className);
     inputsText.val('');
-    inputsCheckbox.attr('checked', false);
+    //inputsCheckbox.attr('checked', false);
     inputsText.attr('disabled', checked);
-    inputsCheckbox.attr('disabled', checked);
+    //inputsCheckbox.attr('disabled', checked);
 }
