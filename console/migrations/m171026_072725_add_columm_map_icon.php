e99f27632faba0351f734c7a764fdac5e8b0d6fc
Max Matiukhin <matuhinmax@mail.ru>
2017-10-27 12:10:08 +0300

    Вывод купонов для отключенных шопов.

--- console/migrations/m171026_072725_add_columm_map_icon.php
+++ console/migrations/m171026_072725_add_columm_map_icon.php
@@ -8,8 +8,6 @@ class m171026_072725_add_columm_map_icon extends Migration
     {
       $this->execute('SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=\'TRADITIONAL,ALLOW_INVALID_DATES\';');
       $this->execute('SET SQL_MODE=\'ALLOW_INVALID_DATES\';');
-      $this->execute('SET @@global.sql_mode ="ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION";');
-      $this->execute('SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,\'ONLY_FULL_GROUP_BY\',\'\'));');
 
       $this->addColumn('cw_categories_stores', 'map_icon', $this->string()->null());
       $this->addColumn('b2b_stores_points', 'category_id', $this->integer()->null()->defaultValue(0));
