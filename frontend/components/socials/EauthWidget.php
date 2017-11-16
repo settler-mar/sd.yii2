675f1580474d36f002d3795239faa5ed25fe474e
Andrey <oxygenn@list.ru>
2017-11-16 13:46:07 +0300

    Шаблон twig для виджета

--- frontend/components/socials/EauthWidget.php
+++ frontend/components/socials/EauthWidget.php
@@ -2,15 +2,38 @@
 
 namespace frontend\components\socials;
 
-use yii\base\Widget;
+//use yii\base\Widget;
+//use yii\web\View;
+//use yii;
 
-class EauthWidget extends Widget
+class EauthWidget extends \nodge\eauth\Widget
 {
   public $vertical = false;
-  
+
+//  public function run()
+//  {
+//    return \nodge\eauth\Widget::widget(['action' => '/socials/login']);
+//  }
+
   public function run()
   {
-    //return \nodge\eauth\Widget::widget(['action' => '/socials/login']);
-    return NodgeWidget::widget(['action' => '/socials/login', 'vertical' => $this->vertical]);
+    $js = false;
+    if ($this->popup) {
+      $options = [];
+      foreach ($this->services as $name => $service) {
+        $options[$service->id] = $service->jsArguments;
+      }
+      $js = 'jQuery("#' . $this->getId() . '").eauth(' . json_encode($options) . ');';
+    }
+
+    echo $this->render('/widgets/socials', [
+      'id' => $this->getId(),
+      'services' => $this->services,
+      'action' => 'login/socials',
+      'popup' => $this->popup,
+      'assetBundle' => $this->assetBundle,
+      'vertical' => $this->vertical,
+      'js' => $js,
+    ]);
   }
 }
