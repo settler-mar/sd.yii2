b756c834b7930bcc26d53bce0c4131d2aae63d44
Max Matiukhin <matuhinmax@mail.ru>
2017-11-20 22:42:19 +0200

    Из общих параметров перенес в локальные параметры почты

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,5 @@
 <?php
 return [
-  //'adminEmail' => 'support@secretdiscounter.com', //почта от имени которой отправлять письма
-  'adminEmail' => 'oxygenn@yandex.ru', //почта от имени которой отправлять письма
-  'adminName' => 'Secret Discounter', //имя от которого отправлять письма с сайта
-  'supportEmail' => 'support@secretdiscounter.com', //сюда письма отправлются с формы поддержки
   'user.passwordResetTokenExpire' => 3600,
   'scriptVersion'=>YII_DEBUG?'':'.min.'.file_get_contents(__DIR__.'/script_version.data').'.',
   'pathToScript'=>require __DIR__.'/path_scripts.php',
