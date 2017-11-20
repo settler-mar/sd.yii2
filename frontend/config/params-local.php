e6ea7c179ff4d0dde907431453101e02d257f62a
Andrey <oxygenn@list.ru>
2017-11-20 22:31:19 +0300

    Параметр promo в реферальной ссылке. В params массив, для записи в поля нового пользователя

--- frontend/config/params-local.php
+++ frontend/config/params-local.php
@@ -1,4 +1,14 @@
 <?php
 return [
   'stores_menu_separate' => 0,
+  'ref_promo' => [
+    'default' => [
+      'bonus_status' => null,
+      'loyalty_status' => 5,
+    ],
+    'premium' => [
+      'bonus_status' => null,
+      'loyalty_status' => 6,
+    ],
+  ],
 ];
