ab2a446139d8e7c9fe7f4d1509327a577b509831
Max Matiukhin <matuhinmax@mail.ru>
2017-11-22 20:05:50 +0200

    Поправил параметры для стартового уровня лояльности

--- frontend/config/params-local.php
+++ frontend/config/params-local.php
@@ -3,11 +3,11 @@ return [
   'stores_menu_separate' => 0,
   'ref_promo' => [
     'default' => [
-      'loyalty_status' => 5,
+      'loyalty_status' => 4,
       'time'=>10,
     ],
     'premium' => [
-      'loyalty_status' => 5,
+      'loyalty_status' => 4,
       'time'=>false
     ],
   ],
