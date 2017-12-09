27395451962dac4f192ed0adec17fe07fac55f45
Andrey <oxygenn@list.ru>
2017-12-09 16:05:02 +0300

    account/welcome.twig

---
+++ console/migrations/m171209_121012_AddConstantVerifyEmail.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+use \frontend\modules\constants\models\Constants;
+
+class m171209_121012_AddConstantVerifyEmail extends Migration
+{
+    public function safeUp()
+    {
+        $const = new Constants();
+        $const->name='account_verify_email';
+        $const->title = 'Аккаунт. Текст подсказки о подтверждении E-mail';
+        $const->text = '<h3>Подтвердите свой E-mail для активации аккаунта</h3>
+                        <p>
+                            На ваш почтовый ящик <b>{{ this.user.email }}</b>
+                            {{ _if(this.user.email_verify_time,_date(this.user.email_verify_time,"%H:%M")~" (МСК)") }}
+                            было отправлено письмо со ссылкой для активации.
+                            Если письмо не пришло в течение 5 минут – проверьте папку «Спам».
+                        </p>
+                        <p>Подтверждение необходимо сделать в течение 15 минут после получения письма.</p>
+                        <p>
+                            <a class="btn btn-yellow-border" href="/account/sendverifyemail" style="margin-right: 20px;">Повторно отправить письмо</a>
+                        </p>';
+        $const->ftype = 'textarea';
+        $const->save();
+    }
+
+    public function safeDown()
+    {
+        $const = Constants::findOne(['name' => 'account_verify_email']);
+        $const->delete();
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m171209_121012_AddConstantVerifyEmail cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
--- frontend/language/en-EN/account.php
+++ frontend/language/en-EN/account.php
@@ -73,5 +73,13 @@ return [
     'menu_withdraw' => 'Вывод денег',
     'menu_settings' => 'Настройки аккаунта',
     'menu_tools' => 'Операции',
+    'view_payments_history' => 'Посмотреть историю покупок',
+    'user_loyalty_status' => 'Ваш статус лояльности',
+    'user_loyalty_current_status' => 'Текущий статус',
+    'for_status {desire} left {left}' => 'Для получения статуса <b>{desire}</b> осталось заработать <span>{left}<span class="fa fa-rub"></span></span>',
+    'loyalty_status_min_sum {tariff_min_summ}' => 'Для вашего аккаунта установлен <strong>персональный уровень лояльности</strong>. <br> Это означает, что после каждой покупки ваш кэшбэк будет увеличен на <strong>{tariff_min_summ}%</strong>.',
+    'premium_status_since {user_loyalty_status_end}' => 'Премиум-аккаунт действует до {user_loyalty_status_end}',
+    'loyalty_vip_client' => 'Вы почётный <strong>VIP</strong> клиент кэшбэк сервиса SecretDiscounter.<br> Если у Вас возник какой-либо вопрос связанный с работой нашего сайта, <br>обратитесь к нам напрямую написав на наш официальный skype - <strong>secretdiscounter.official</strong>.',
+
 
 ];
