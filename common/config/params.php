d0720ce371fc999ef4d9cfd7a11fd5cfa6fe33a2
Andrey <oxygenn@list.ru>
2017-12-11 14:56:37 +0300

    Перевод валюты

--- common/config/params.php
+++ common/config/params.php
@@ -3,5 +3,32 @@ return [
   'user.passwordResetTokenExpire' => 3600,
   'scriptVersion'=>YII_DEBUG?'':'.min.'.file_get_contents(__DIR__.'/script_version.data').'.',
   'pathToScript'=>require __DIR__.'/path_scripts.php',
-  'exportDir' => 'export'
+  'exportDir' => 'export',
+
+  'currencies' => [
+      'RUB' => [
+          'text' => 'руб',
+          'icon' => '<span class="fa fa-rub"></span>',
+      ],
+      'USD' => [
+          'text' => 'usd',
+          'icon' => '<span class="fa fa-usd"></span>',
+      ],
+      'EUR' => [
+          'text' => 'eur',
+          'icon' => '<span class="fa fa-eur"></span>',
+      ],
+      'KZT' => [
+          'text' => 'kzt',
+          'icon' => 'kzt',
+      ],
+      'UAH' => [
+          'text' => 'гр',
+          'icon' => 'гр',
+      ],
+      'BYN' => [
+          'text' => 'бел.руб',
+          'icon' => 'бел.руб',
+      ],
+  ]
 ];
