8a57df2400b21347073d507d24ddadadde00a8c5
Max Matiukhin <matuhinmax@mail.ru>
2017-12-11 12:22:51 +0200

    Правки миграции(проверка на существование файла с банером)

--- console/migrations/m171208_170339_AddAccountBanner.php
+++ console/migrations/m171208_170339_AddAccountBanner.php
@@ -5,45 +5,46 @@ use frontend\modules\banners\models\Banners;
 
 class m171208_170339_AddAccountBanner extends Migration
 {
-    public function safeUp()
-    {
-      $this->execute('SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=\'TRADITIONAL,ALLOW_INVALID_DATES\';');
-      $this->execute('SET SQL_MODE=\'ALLOW_INVALID_DATES\';');
+  public function safeUp()
+  {
+    $this->execute('SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=\'TRADITIONAL,ALLOW_INVALID_DATES\';');
+    $this->execute('SET SQL_MODE=\'ALLOW_INVALID_DATES\';');
 
-        $banner = new Banners();
-        $banner->url = 'account/affiliate';
-        $banner->picture = 'tel_friend.png';
-        $banner->places = 'account-left-menu';
-        $banner->new_window =  0;
-        $banner->is_active = 1;
-        $banner->save();
+    $banner = new Banners();
+    $banner->url = 'account/affiliate';
+    $banner->picture = 'tel_friend.png';
+    $banner->places = 'account-left-menu';
+    $banner->new_window = 0;
+    $banner->is_active = 1;
+    $banner->save();
 
-        copy(
-            Yii::$app->getBasePath() . '/../frontend/web/images/account/tel_friend.png',
-            Yii::$app->getBasePath() . '/../frontend/web/images/banners/tel_friend.png'
-        );
-
+    if (is_readable(Yii::$app->getBasePath() . '/../frontend/web/images/account/tel_friend.png')) {
+      copy(
+        Yii::$app->getBasePath() . '/../frontend/web/images/account/tel_friend.png',
+        Yii::$app->getBasePath() . '/../frontend/web/images/banners/tel_friend.png'
+      );
     }
+  }
 
-    public function safeDown()
-    {
-        echo "m171208_170339_AddAccountBanner cannot be reverted.\n";
+  public function safeDown()
+  {
+    echo "m171208_170339_AddAccountBanner cannot be reverted.\n";
 
-        return false;
-    }
+    return false;
+  }
 
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
+  /*
+  // Use up()/down() to run migration code without a transaction.
+  public function up()
+  {
 
-    }
+  }
 
-    public function down()
-    {
-        echo "m171208_170339_AddAccountBanner cannot be reverted.\n";
+  public function down()
+  {
+      echo "m171208_170339_AddAccountBanner cannot be reverted.\n";
 
-        return false;
-    }
-    */
+      return false;
+  }
+  */
 }
