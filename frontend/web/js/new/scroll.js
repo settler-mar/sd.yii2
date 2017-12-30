2d36b45765f5343d130525db7eb626ddf5899e9a
Max Matiukhin <matuhinmax@mail.ru>
2017-12-30 21:24:48 +0200

    Добавил автоскрол

---
+++ frontend/web/js/new/scroll.js
@@ -0,0 +1,145 @@
+(function (w, d, $) {
+    var scrolls_block = $('.scroll_box');
+
+    if(scrolls_block.length==0) return;
+    $('<div class="scroll_box-wrap"/>').wrapAll(scrolls_block);
+
+    init_scroll();
+    calc_scroll();
+
+    $(window).resize(function () {
+        calc_scroll();
+    });
+
+    function init_scroll() {
+        var control = '<div class="scroll_box-control"></div>';
+        control=$(control);
+        control.insertAfter(scrolls_block);
+        control.data('slide-active', 0);
+
+        scrolls_block.prepend('<div class=scroll_box-mover></div>');
+
+        control.on('click','.scroll_box-control_point',function () {
+            var $this = $(this);
+            var control = $this.parent();
+            var i = $this.index();
+            if($this.hasClass('active'))return;
+            control.find('.active').removeClass('active');
+            $this.addClass('active');
+
+            var dx=control.data('slide-dx');
+            var el = control.prev();
+            el.find('.scroll_box-mover').css('margin-left',-dx*i);
+            control.data('slide-active', i);
+
+            stopScrol.bind(el)();
+        })
+    }
+
+    for (var j = 0; j < scrolls_block.length; j++) {
+        var el = scrolls_block.eq(j);
+        el.parent().hover(stopScrol.bind(el),startScrol.bind(el));
+    }
+
+    function startScrol(){
+        var $this=$(this);
+        if(!$this.hasClass("scroll_box-active"))return;
+
+        var timeoutId = setTimeout(next_slide.bind($this), 2000);
+        $this.data('slide-timeoutId',timeoutId)
+    }
+
+    function stopScrol(){
+        var $this=$(this);
+        var timeoutId=$this.data('slide-timeoutId');
+        $this.data('slide-timeoutId',false);
+        if(!$this.hasClass("scroll_box-active") || !timeoutId)return;
+        clearTimeout(timeoutId);
+    }
+
+    function next_slide() {
+        var $this=$(this);
+        $this.data('slide-timeoutId',false)
+        if(!$this.hasClass("scroll_box-active"))return;
+
+        var controls=$this.next().find('>*');
+        var active=$this.data('slide-active');
+        var point_cnt=controls.length;
+        if(!active)active=0;
+        active++;
+        if(active>=point_cnt)active=0;
+        $this.data('slide-active', active);
+
+        controls.eq(active).click();
+        startScrol.bind($this)();
+    }
+
+    function calc_scroll() {
+        for(i=0;i<scrolls_block.length;i++) {
+            var el = scrolls_block.eq(i);
+            var control = el.next();
+            var width_max = el.data('scroll-width-max');
+            w = el.width();
+
+            //делаем контроль ограничения ширины. Если превышено то отключаем скрол и переходим к следующему элементу
+            if (width_max && w > width_max) {
+                control.removeClass("scroll_box-active");
+                control.data('slide-active', 0); //сброс счетчика
+                continue;
+            }
+
+            var no_class = el.data('scroll-elemet-ignore-class');
+            var children = el.find('>*').not('.scroll_box-mover');
+            if (no_class) {
+                children = children.not('.' + no_class)
+            }
+
+            //Если нет дочерних для скрола
+            if (children == 0) {
+                el.removeClass("scroll_box-active");
+                control.data('slide-active', 0); //сброс счетчика
+                continue;
+            }
+
+            var f_el=children.eq(1);
+            var children_w = f_el.outerWidth(); //всего дочерних для скрола
+            children_w+=parseFloat(f_el.css('margin-left'));
+            children_w+=parseFloat(f_el.css('margin-right'));
+
+            var screan_count = Math.floor(w / children_w);
+
+            //Если все влазит на экран
+            if (children <= screan_count) {
+                el.removeClass("scroll_box-active");
+                control.data('slide-active', 0); //сброс счетчика
+                continue;
+            }
+
+            //Уже точно знаем что скрол нужен
+            el.addClass("scroll_box-active");
+
+            var point_cnt = children.length - screan_count + 1;
+            //если не надо обновлять контрол то выходим
+            if (control.find('>*').length == point_cnt) {
+                continue;
+            }
+
+            active = el.data('slide-active');
+            if (!active)active = 0;
+            if (active >= point_cnt)active = point_cnt - 1;
+            var out = '';
+            for (var j = 0; j < point_cnt; j++) {
+                out += '<div class="scroll_box-control_point'+(j==active?' active':'')+'"></div>';
+            }
+            control.html(out);
+
+            control.data('slide-active', active);
+            control.data('slide-count', point_cnt);
+            control.data('slide-dx', children_w);
+
+            if(!el.data('slide-timeoutId')){
+                startScrol.bind(el)();
+            }
+        }
+    }
+}(window, document, jQuery));
