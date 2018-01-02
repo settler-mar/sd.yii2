fd6a9248617889d297cf2918959358eaf9adc512
Max Matiukhin <matuhinmax@mail.ru>
2018-01-02 13:19:00 +0200

    Добавил блок вопрос-ответ и Все еще сомниваетесь

---
+++ frontend/web/js/new/accordion.js
@@ -0,0 +1,33 @@
+var accordionControl = $('.accordion .accordion-control');
+
+accordionControl.on('click', function (e) {
+    e.preventDefault();
+    $this = $(this);
+    $accordion = $this.closest('.accordion');
+
+    if ($accordion.hasClass('open')) {
+        if($accordion.hasClass('accordion-slim')){
+            return false;
+        }
+        $accordion.find('.accordion-content').hide(300);
+        $accordion.removeClass('open')
+    } else {
+        if($accordion.hasClass('accordion-slim')){
+            $other=$accordion.parent().find('.accordion-slim');
+            $other.find('.accordion-content').hide(300);
+            $other.removeClass('open')
+        }
+        $accordion.find('.accordion-content').show(300);
+        $accordion.addClass('open')
+    }
+    return false;
+});
+accordionControl.show();
+
+//для cbvjd jnrhsdftv 1-й
+accordionSlim=$('.accordion.accordion-slim');
+if(accordionSlim.length>0){
+    accordionSlim.parent().find('.accordion-slim:first-child')
+        .addClass('open')
+        .find('.accordion-content').show(300);
+}
