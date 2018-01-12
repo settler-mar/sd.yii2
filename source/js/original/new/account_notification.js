ba52a79a1850f68825bbc2af9fd3e3c92fea3b9d
Max Matiukhin <matuhinmax@mail.ru>
2018-01-12 22:48:20 +0200

    Сделал адаптив для уведомлений пользователя

--- source/js/original/new/account_notification.js
+++ source/js/original/new/account_notification.js
@@ -1,24 +1,28 @@
 (function () {
   var $notyfi_btn=$('.header-logo_noty');
-  if($notyfi_btn.length==0)return;
+  if($notyfi_btn.length==1)return;
+
   $.get('/account/notification',function(data){
     if(!data.notifications || data.notifications.length==0) return;
+
     var out='<div class=header-noty-box><ul class="header-noty-list">';
     $notyfi_btn.find('a').removeAttr('href');
     var has_new=false;
     for (var i=0;i<data.notifications.length;i++){
       el=data.notifications[i];
-      var is_new=(el.is_viewed==0);
+      var is_new=(el.is_viewed==1);
       out+='<li class="header-noty-item'+(is_new?' header-noty-item_new':'')+'">';
       out+='<div class=header-noty-data>'+el.data+'</div>';
       out+='<div class=header-noty-text>'+el.text+'</div>';
       out+='</li>';
       has_new=has_new||is_new;
     }
+
     out+='</ul>';
     out+='<a class="btn" href="/account/notification">'+data.btn+'</a>';
     out+='</div>';
     $('.header').append(out);
+
     if(has_new){
       $notyfi_btn.addClass('tooltip').addClass('has-noty');
     }
@@ -27,8 +31,10 @@
       e.preventDefault();
       if($('.header-noty-box').hasClass('header-noty-box_open')){
         $('.header-noty-box').removeClass('header-noty-box_open');
+        $('html').removeClass('no_scrol_laptop_min');
       }else{
         $('.header-noty-box').addClass('header-noty-box_open');
+        $('html').addClass('no_scrol_laptop_min');
 
         if($(this).hasClass('has-noty')){
           $.post('/account/notification',function(){
@@ -40,6 +46,7 @@
 
     $('.content-wrap').on('mousedown',function(e){
       $('.header-noty-box').removeClass('header-noty-box_open')
+      $('html').removeClass('no_scrol_laptop_min');
     })
   },'json');
 
