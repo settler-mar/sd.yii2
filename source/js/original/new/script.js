8d05b767bb025875aa2105dc1f600299e0c753e1
Andrey <oxygenn@list.ru>
2018-01-13 16:43:46 +0300

    Верхняя панель

--- source/js/original/new/script.js
+++ source/js/original/new/script.js
@@ -32,6 +32,8 @@ var headerActions = function () {
     $('.tooltip').tipso({
         background : '#4A4A4A',
         size: 'small',
+        delay: 100,
+        speed: 100,
         onBeforeShow : function(ele, tipso) {
             this.content = ele.data('tooltip');
         }
