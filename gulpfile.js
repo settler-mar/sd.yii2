4090236e0e232d0dc8d8335ed3f0a2c10f16a612
Max Matiukhin <matuhinmax@mail.ru>
2018-01-15 13:33:21 +0200

    Избранное

--- gulpfile.js
+++ gulpfile.js
@@ -128,6 +128,7 @@ gulp.task('jsaccount', compileJs([
         paths.source.js+'/original/notification.js',
         paths.source.js+'/original/admin/ajax_remove.js',
         paths.source.js+'/original/language.js',
+        paths.source.js+'/original/new/favorites.js',
         paths.source.js+'/original/for_all.js'
     ], paths.app.js + '/account')
 );
@@ -174,6 +175,7 @@ gulp.task('js_new', compileJs([
         paths.source.js+'/original/new/footer_menu.js',
         paths.source.js+'/original/new/rating.js',
         paths.source.js+'/original/new/account_notification.js',
+        paths.source.js+'/original/new/favorites.js',
         paths.source.js+'/original/new/script.js'
     ], paths.app.js+'/new')
 );
