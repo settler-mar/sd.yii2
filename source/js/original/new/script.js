41f6d172021677ba43aaf492db586f2dd5ba571e
Andrey <oxygenn@list.ru>
2018-01-16 10:08:56 +0300

    Tooltip

--- source/js/original/new/script.js
+++ source/js/original/new/script.js
@@ -42,15 +42,6 @@ var headerActions = function () {
         scrolledDown = false;
     });
 
-    $('.tooltip').tipso({
-        background : '#4A4A4A',
-        size: 'small',
-        delay: 100,
-        speed: 100,
-        onBeforeShow : function(ele, tipso) {
-            this.content = ele.data('tooltip');
-        }
-    });
 
     $('[data-toggle=tooltip]').tipso({
         background : '#4A4A4A',
