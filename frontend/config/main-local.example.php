aae588f280e2a5d4808ff245e9fa4c3f409419e1
Max Matiukhin <matuhinmax@mail.ru>
2018-01-18 09:48:05 +0200

    /frontend/config/main-local.php в гитигнор

---
+++ frontend/config/main-local.example.php
@@ -0,0 +1,18 @@
+<?php
+$config = [
+  'components' => [
+    'request' => [
+      // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+      'cookieValidationKey' => 'JA6Z4ogm9gRIe6S-nICb-OzZOTXNVhMB',
+    ],
+  ],
+  'db_blog' => [
+      'class' => 'yii\db\Connection',
+      'dsn' => 'mysql:host=localhost;dbname=sdblog',
+      'username' => 'root',
+      'password' => '123456',
+      'charset' => 'utf8',
+  ],
+];
+
+return $config;
