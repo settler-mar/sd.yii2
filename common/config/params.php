f6d4a59ae4b50e6198137197c9f70cd3e709fc8f
Andrey <oxygenn@list.ru>
2018-02-07 18:47:19 +0300

    Иконки в кнопках пагинации. Поправил вывод точек

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,8 @@
 <?php
 return [
   'user.passwordResetTokenExpire' => 3600,
-  'scriptVersion'=>YII_DEBUG?'':'.min.'.file_get_contents(__DIR__.'/script_version.data').'.',
+  //'scriptVersion'=>YII_DEBUG?'':'.min.'.file_get_contents(__DIR__.'/script_version.data').'.',
+  'scriptVersion'=>YII_DEBUG?'' : '.min.',
   'pathToScript'=>require __DIR__.'/path_scripts.php',
   'exportDir' => 'export',
 
