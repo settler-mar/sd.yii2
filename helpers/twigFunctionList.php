c694944e7669a03e1646bf1af15058905e66ecde
Max Matiukhin <matuhinmax@mail.ru>
2018-02-08 12:06:32 +0200

    Оформил вывод баланса

--- helpers/twigFunctionList.php
+++ helpers/twigFunctionList.php
@@ -191,6 +191,12 @@ $functionsList=[
       }
     });
   },
+  'currencyIcon'=> function ($currency) use ($currencyIcon) {
+    return (isset($currencyIcon[$currency]) ? Help::svg(
+        $currencyIcon[$currency],
+        'currency-icon currency-icon-'.$currencyIcon[$currency]
+    ) : $currency);
+  },
   //функция - вывести кешбек  и валюту, если не задан процента кешбека для шопа
   '_cashback'=> function ($cashback, $currency='',$action = 0,$mode = 0) use ($currencyIcon) {
     if($action == 1){
