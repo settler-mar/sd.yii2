f9f145c552e43127e91711c1ae223ba55ffdda79
Andrey <oxygenn@list.ru>
2018-02-08 21:40:25 +0300

    Стилизация сообщения

--- source/js/original/new/goto.js
+++ source/js/original/new/goto.js
@@ -1,10 +1,17 @@
 (function(){
+
     $('.coupons-list_item-content-goto-promocode-link').click(function(e){
         var expired = $(this).closest('.coupons-list_item').find('.clock-expired');
         if (expired.length > 0) {
             var title = 'К сожалению, срок действия данного промокода истек.';
             var message = 'Все действующие промокоды вы можете посмотреть <a href="/coupons">здесь</a>';
-            notification.alert({'title': title, 'question': message, 'buttonYes': 'Закрыть', 'buttonNo':false });
+            notification.alert({
+                'title': title,
+                'question': message,
+                'buttonYes': false,
+                'buttonNo': false,
+                'notyfy_class': 'notify_box-alert'
+            });
             return false;
         }
     });
