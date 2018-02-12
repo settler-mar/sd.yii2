f82236095184fb184073378855b8fbc69cf00259
Andrey <oxygenn@list.ru>
2018-02-12 09:09:04 +0300

    Сообщение при клике на просроченный купон

--- source/js/original/new/goto.js
+++ source/js/original/new/goto.js
@@ -3,8 +3,8 @@
     $('.coupons-list_item-content-goto-promocode-link').click(function(e){
         var expired = $(this).closest('.coupons-list_item').find('.clock-expired');
         if (expired.length > 0) {
-            var title = 'К сожалению, срок действия данного промокода истек.';
-            var message = 'Все действующие промокоды вы можете посмотреть <a href="/coupons">здесь</a>';
+            var title = 'К сожалению, срок действия данного промокода истек';
+            var message = 'Все действующие промокоды вы можете <a href="/coupons">посмотреть здесь</a>';
             notification.alert({
                 'title': title,
                 'question': message,
