a38589f7ce1ddd29da9971e26e7a3e1ff0a4c571
Andrey <oxygenn@list.ru>
2018-02-13 10:07:21 +0300

    Левое меню в мобиле в положении бутерброд

--- source/js/original/new/left-menu-toggle.js
+++ source/js/original/new/left-menu-toggle.js
@@ -3,4 +3,8 @@
        $('.catalog-categories_tree-cat').toggleClass('catalog-categories_tree-cat-open');
        $('.catalog-categories_tree-toggle').toggleClass('catalog-categories_tree-toggle-open');
    });
+   $('.catalog-categories_tree-cat-close').on('click', function(){
+       $('.catalog-categories_tree-cat').toggleClass('catalog-categories_tree-cat-open');
+       $('.catalog-categories_tree-toggle').toggleClass('catalog-categories_tree-toggle-open');
+   });
 })();
