45eac999052c133ff59cc2820c0ed39f24d118b6
Max Matiukhin <matuhinmax@mail.ru>
2018-02-14 16:00:11 +0200

    Добавил адаптацию для таблиц

---
+++ source/js/original/new/table.js
@@ -0,0 +1,27 @@
+(function (window, document) {
+  "use strict";
+
+  var tables = $('table.adaptive');
+
+  if (tables.length == 0)return;
+
+  for (var i = 0; tables.length > i; i++) {
+    var table = tables.eq(i);
+    var th = table.find('thead');
+    if (th.length == 0) {
+      th = table.find('tr').eq(0);
+    } else {
+      th = th.find('tr').eq(0);
+    }
+    th = th.addClass('table-header').find('td,th');
+
+    var tr = table.find('tr').not('.table-header');
+
+    for (var j = 0; j < th.length; j++) {
+      var k=j+1;
+      var td = tr.find('td:nth-child('+k+')');
+      td.attr('label',th.eq(j).text());
+    }
+  }
+
+})(window, document);
