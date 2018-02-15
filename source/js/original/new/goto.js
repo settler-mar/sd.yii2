799432f4cfc1c7caff393282cecf9fb535f8f1e9
Andrey <oxygenn@list.ru>
2018-02-15 15:48:36 +0300

    Модалка перед использованием купона для незарегистрированного

--- source/js/original/new/goto.js
+++ source/js/original/new/goto.js
@@ -1,7 +1,9 @@
 (function(){
 
     $('.coupons-list_item-content-goto-promocode-link').click(function(e){
-        var expired = $(this).closest('.coupons-list_item').find('.clock-expired');
+        var that = $(this);
+        var expired = that.closest('.coupons-list_item').find('.clock-expired');
+        var userId = $(that).data('user');
         if (expired.length > 0) {
             var title = 'К сожалению, срок действия данного промокода истек';
             var message = 'Все действующие промокоды вы можете <a href="/coupons">посмотреть здесь</a>';
@@ -12,6 +14,24 @@
                 'buttonNo': false,
                 'notyfy_class': 'notify_box-alert'
             });
+            return false;
+        }
+        if (!userId) {
+            var data={
+                'buttonYes':false,
+                'notyfy_class': "notify_box-alert",
+                'title': 'Использовать промокод',
+                'question':
+                    '<div class="notify_box-coupon-noregister">'+
+                        '<img src="/images/templates/swa.png" alt="">'+
+                        '<p><b>Если вы хотите получать еще и КЭШБЭК (возврат денег), вам необходимо зарегистрироваться. Но можете и просто воспользоваться промокодом, без кэшбэка.</b></p>'+
+                    '</div>'+
+                    '<div class="notify_box-buttons">'+
+                        '<a href="'+that.attr('href')+'" target="_blank" class="btn">Использовать промокод</a>'+
+                        '<a href="#registration" class="btn btn-transform modals_open">Зарегистрироваться</a>'+
+                    '</div>'
+            };
+            notification.alert(data);
             return false;
         }
     });
