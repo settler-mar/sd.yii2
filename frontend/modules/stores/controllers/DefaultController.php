0a24dba7f35931fc47e7af6f9f96a3afee00974b
Andrey <oxygenn@list.ru>
2018-02-16 10:50:26 +0300

    Изменил вывод категории

--- frontend/modules/stores/controllers/DefaultController.php
+++ frontend/modules/stores/controllers/DefaultController.php
@@ -38,35 +38,35 @@ class DefaultController extends SdController
    */
   public function createAction($id)
   {
-     $request = \Yii::$app->request;
+    $request = \Yii::$app->request;
     //здесь могут быть редиректы со старых роутов, поэтому делаем проверку для параметров старых роутов
     //передаваемых в запросе быть не должно
     $this->checkWrongParams(['store', 'expired', 'coupon', 'id']);
     $category = $request->get('category');
-//        $store = $request->get('store');
-//        if ($store) {
-//            throw new \yii\web\NotFoundHttpException;
-//        }
+
     if ($category) {
       $this->routeRedirects($category);
       exit;
     }
     if ($id) {
       //имеется action, который должен быть категорией или магазином, ищем такую
-      if (strpos($id, '-offline') == strlen($id) - strlen('-offline')) {
-          //если в конце категории слово -offline
-          $id = substr($id, 0, strlen($id) - strlen('-offline'));
-          $this->offline = 1;
-      }
+
+       //если в конце категории или шопа слово -offline
+      $this->offline = strpos($id, '-offline') == strlen($id) - strlen('-offline');
+
       $store = Stores::byRoute($id);
       if ($store) {
         //есть магазин
         //$this->Params($request->get(), ['page']);
         $cpaLink=$store->cpaLink;
-        if ($cpaLink && $cpaLink->cpa_id == 2) {
-          //online-offline шоп, для поиска мететегов добавляем в url-mask
-          \Yii::$app->params['url_mask'] = $request->pathInfo . '/online';
-        }
+//        if ($cpaLink && $cpaLink->cpa_id == 2) {
+//          //online-offline шоп, для поиска мететегов добавляем в url-mask
+//          \Yii::$app->params['url_mask'] = $request->pathInfo . '/online';
+//        }
+        \Yii::$app->params['url_mask'] = 'stores/*' .
+            ($cpaLink && $cpaLink->cpa_id == 1 ? '/online' : '').
+            ($this->offline ? '/offline' : '');
+
         $this->checkParams('store');
         echo $this->actionStore($store);
         exit;
@@ -74,7 +74,7 @@ class DefaultController extends SdController
       $categoryStore = CategoriesStores::byRoute($id);
       if ($categoryStore) {
         //если есть категория
-        \Yii::$app->params['url_mask'] = 'stores/category/' . $id;
+        \Yii::$app->params['url_mask'] = 'stores/category/*' .($this->offline ? '/offline' : '');
         echo $this->actionIndex($id, $categoryStore);
         exit;
       };
--- frontend/modules/stores/models/CategoriesStores.php
+++ frontend/modules/stores/models/CategoriesStores.php
@@ -298,6 +298,10 @@ class CategoriesStores extends \yii\db\ActiveRecord
    */
   public static function byRoute($route)
   {
+    if (strpos($route, '-offline') == strlen($route) - strlen('-offline')) {
+          //если в конце категории слово -offline
+          $route = substr($route, 0, strlen($route) - strlen('-offline'));
+    }
     $cache = \Yii::$app->cache;
     $dependency = new yii\caching\DbDependency;
     $dependencyName = 'catalog_stores';
