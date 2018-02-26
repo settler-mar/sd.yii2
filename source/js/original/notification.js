ac0b22746d212eba9c994d6cd035d0a6a87f8f73
Max Matiukhin <matuhinmax@mail.ru>
2018-02-26 19:06:02 +0200

    Тест фикса курсора на айфоне v3

--- source/js/original/notification.js
+++ source/js/original/notification.js
@@ -1,185 +1,192 @@
-var notification = (function() {
+var notification = (function () {
   var conteiner;
   var mouseOver = 0;
   var timerClearAll = null;
   var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
   var time = 10000;
 
-  var notification_box =false;
-  var is_init=false;
-  var confirm_opt={
-    title:"Удаление",
-    question:"Вы действительно хотите удалить?",
-    buttonYes:"Да",
-    buttonNo:"Нет",
-    callbackYes:false,
-    callbackNo:false,
-    obj:false,
-    buttonTag:'div',
-    buttonYesDop:'',
-    buttonNoDop:'',
+  var notification_box = false;
+  var is_init = false;
+  var confirm_opt = {
+    title: "Удаление",
+    question: "Вы действительно хотите удалить?",
+    buttonYes: "Да",
+    buttonNo: "Нет",
+    callbackYes: false,
+    callbackNo: false,
+    obj: false,
+    buttonTag: 'div',
+    buttonYesDop: '',
+    buttonNoDop: '',
   };
-  var alert_opt={
-    title:"",
-    question:"Сообщение",
-    buttonYes:"Да",
-    callbackYes:false,
-    buttonTag:'div',
-    obj:false,
+  var alert_opt = {
+    title: "",
+    question: "Сообщение",
+    buttonYes: "Да",
+    callbackYes: false,
+    buttonTag: 'div',
+    obj: false,
   };
 
-
-  function init(){
-    is_init=true;
-    notification_box=$('.notification_box');
-    if(notification_box.length>0)return;
-
-    $('body').append("<div class='notification_box'></div>");
-    notification_box=$('.notification_box');
-
-    notification_box.on('click','.notify_control',closeModal);
-    notification_box.on('click','.notify_close',closeModal);
-    notification_box.on('click',closeModalFon);
+  function testIphone() {
+    if (!/(iPhone|iPad|iPod).*(OS 11)/.test(navigator.userAgent)) return
+    notification_box.css('position', 'absolute');
+    notification_box.css('top', $(document).scrollTop());
   }
 
-  function closeModal(){
+  function init() {
+    is_init = true;
+    notification_box = $('.notification_box');
+    if (notification_box.length > 0)return;
+
+    $('body').append("<div class='notification_box'></div>");
+    notification_box = $('.notification_box');
+
+    notification_box.on('click', '.notify_control', closeModal);
+    notification_box.on('click', '.notify_close', closeModal);
+    notification_box.on('click', closeModalFon);
+  }
+
+  function closeModal() {
     $('html').removeClass('show_notifi');
     $('.notification_box .notify_content').html('')
   }
 
-  function closeModalFon(e){
+  function closeModalFon(e) {
     var target = e.target || e.srcElement;
-    if(target.className=="notification_box"){
+    if (target.className == "notification_box") {
       closeModal();
     }
   }
 
-  var _setUpListeners = function() {
+  var _setUpListeners = function () {
     $('body').on('click', '.notification_close', _closePopup);
     $('body').on('mouseenter', '.notification_container', _onEnter);
     $('body').on('mouseleave', '.notification_container', _onLeave);
   };
 
-  var _onEnter = function(event) {
-    if(event)event.preventDefault();
-    if (timerClearAll!=null) {
+  var _onEnter = function (event) {
+    if (event)event.preventDefault();
+    if (timerClearAll != null) {
       clearTimeout(timerClearAll);
       timerClearAll = null;
     }
-    conteiner.find('.notification_item').each(function(i){
-      var option=$(this).data('option');
-      if(option.timer) {
+    conteiner.find('.notification_item').each(function (i) {
+      var option = $(this).data('option');
+      if (option.timer) {
         clearTimeout(option.timer);
       }
     });
     mouseOver = 1;
   };
 
-  var _onLeave = function() {
-    conteiner.find('.notification_item').each(function(i){
-      $this=$(this);
-      var option=$this.data('option');
-      if(option.time>0) {
+  var _onLeave = function () {
+    conteiner.find('.notification_item').each(function (i) {
+      $this = $(this);
+      var option = $this.data('option');
+      if (option.time > 0) {
         option.timer = setTimeout(_closePopup.bind(option.close), option.time - 1500 + 100 * i);
-        $this.data('option',option)
+        $this.data('option', option)
       }
     });
     mouseOver = 0;
   };
 
-  var _closePopup = function(event) {
-    if(event)event.preventDefault();
+  var _closePopup = function (event) {
+    if (event)event.preventDefault();
 
     var $this = $(this).parent();
-    $this.on(animationEnd, function() {
+    $this.on(animationEnd, function () {
       $(this).remove();
     });
     $this.addClass('notification_hide')
   };
 
-  function alert(data){
-    if(!data)data={};
-    data=objects(alert_opt,data);
+  function alert(data) {
+    if (!data)data = {};
+    data = objects(alert_opt, data);
 
-    if(!is_init)init();
+    if (!is_init)init();
+    testIphone();
 
-    notyfy_class='notify_box ';
-    if(data.notyfy_class)notyfy_class+=data.notyfy_class;
+    notyfy_class = 'notify_box ';
+    if (data.notyfy_class)notyfy_class += data.notyfy_class;
 
-    box_html='<div class="'+notyfy_class+'">';
-    box_html+='<div class="notify_title">';
-    box_html+=data.title;
-    box_html+='<span class="notify_close"></span>';
-    box_html+='</div>';
+    box_html = '<div class="' + notyfy_class + '">';
+    box_html += '<div class="notify_title">';
+    box_html += data.title;
+    box_html += '<span class="notify_close"></span>';
+    box_html += '</div>';
 
-    box_html+='<div class="notify_content">';
-    box_html+=data.question;
-    box_html+='</div>';
+    box_html += '<div class="notify_content">';
+    box_html += data.question;
+    box_html += '</div>';
 
-    if(data.buttonYes||data.buttonNo) {
+    if (data.buttonYes || data.buttonNo) {
       box_html += '<div class="notify_control">';
-      if (data.buttonYes)box_html += '<'+data.buttonTag+' class="notify_btn_yes" '+data.buttonYesDop+'>' + data.buttonYes + '</'+data.buttonTag+'>';
-      if (data.buttonNo)box_html += '<'+data.buttonTag+' class="notify_btn_no" '+data.buttonNoDop+'>' + data.buttonNo + '</'+data.buttonTag+'>';
+      if (data.buttonYes)box_html += '<' + data.buttonTag + ' class="notify_btn_yes" ' + data.buttonYesDop + '>' + data.buttonYes + '</' + data.buttonTag + '>';
+      if (data.buttonNo)box_html += '<' + data.buttonTag + ' class="notify_btn_no" ' + data.buttonNoDop + '>' + data.buttonNo + '</' + data.buttonTag + '>';
       box_html += '</div>';
-    };
+    }
+    ;
 
-    box_html+='</div>';
+    box_html += '</div>';
     notification_box.html(box_html);
 
 
-    setTimeout(function() {
+    setTimeout(function () {
       $('html').addClass('show_notifi');
-    },100)
+    }, 100)
   }
 
-  function confirm(data){
-    if(!data)data={};
-    data=objects(confirm_opt,data);
+  function confirm(data) {
+    if (!data)data = {};
+    data = objects(confirm_opt, data);
 
-    if(!is_init)init();
-
+    if (!is_init)init();
+    testIphone();
     //box_html='<div class="notify_box">';
 
-    notyfy_class='notify_box ';
-    if(data.notyfy_class)notyfy_class+=data.notyfy_class;
+    notyfy_class = 'notify_box ';
+    if (data.notyfy_class)notyfy_class += data.notyfy_class;
 
-    box_html='<div class="'+notyfy_class+'">';
+    box_html = '<div class="' + notyfy_class + '">';
 
-    box_html+='<div class="notify_title">';
-    box_html+=data.title;
-    box_html+='<span class="notify_close"></span>';
-    box_html+='</div>';
+    box_html += '<div class="notify_title">';
+    box_html += data.title;
+    box_html += '<span class="notify_close"></span>';
+    box_html += '</div>';
 
-    box_html+='<div class="notify_content">';
-    box_html+=data.question;
-    box_html+='</div>';
+    box_html += '<div class="notify_content">';
+    box_html += data.question;
+    box_html += '</div>';
 
-    if(data.buttonYes||data.buttonNo) {
+    if (data.buttonYes || data.buttonNo) {
       box_html += '<div class="notify_control">';
       if (data.buttonYes)box_html += '<div class="notify_btn_yes">' + data.buttonYes + '</div>';
       if (data.buttonNo)box_html += '<div class="notify_btn_no">' + data.buttonNo + '</div>';
       box_html += '</div>';
     }
 
-    box_html+='</div>';
+    box_html += '</div>';
     notification_box.html(box_html);
 
-    if(data.callbackYes!=false){
-      notification_box.find('.notify_btn_yes').on('click',data.callbackYes.bind(data.obj));
+    if (data.callbackYes != false) {
+      notification_box.find('.notify_btn_yes').on('click', data.callbackYes.bind(data.obj));
     }
-    if(data.callbackNo!=false){
-      notification_box.find('.notify_btn_no').on('click',data.callbackNo.bind(data.obj));
+    if (data.callbackNo != false) {
+      notification_box.find('.notify_btn_no').on('click', data.callbackNo.bind(data.obj));
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
       $('html').addClass('show_notifi');
-    },100)
+    }, 100)
 
   }
 
   function notifi(data) {
-    if(!data)data={};
-    var option = {time : (data.time||data.time===0)?data.time:time};
+    if (!data)data = {};
+    var option = {time: (data.time || data.time === 0) ? data.time : time};
     if (!conteiner) {
       conteiner = $('<ul/>', {
         'class': 'notification_container'
@@ -193,21 +200,21 @@ var notification = (function() {
       class: 'notification_item'
     });
 
-    if (data.type){
+    if (data.type) {
       li.addClass('notification_item-' + data.type);
     }
 
-    var close=$('<span/>',{
-      class:'notification_close'
+    var close = $('<span/>', {
+      class: 'notification_close'
     });
-    option.close=close;
+    option.close = close;
     li.append(close);
 
-    var content = $('<div/>',{
-      class:"notification_content"
+    var content = $('<div/>', {
+      class: "notification_content"
     });
 
-    if(data.title && data.title.length>0) {
+    if (data.title && data.title.length > 0) {
       var title = $('<h5/>', {
         class: "notification_title"
       });
@@ -215,16 +222,16 @@ var notification = (function() {
       content.append(title);
     }
 
-    var text= $('<div/>',{
-      class:"notification_text"
+    var text = $('<div/>', {
+      class: "notification_text"
     });
     text.html(data.message);
 
-    if(data.img && data.img.length>0) {
+    if (data.img && data.img.length > 0) {
       var img = $('<div/>', {
         class: "notification_img"
       });
-      img.css('background-image','url('+data.img+')');
+      img.css('background-image', 'url(' + data.img + ')');
       var wrap = $('<div/>', {
         class: "wrap"
       });
@@ -232,7 +239,7 @@ var notification = (function() {
       wrap.append(img);
       wrap.append(text);
       content.append(wrap);
-    }else{
+    } else {
       content.append(text);
     }
     li.append(content);
@@ -261,12 +268,12 @@ var notification = (function() {
     //
     // li.append(content);
     //
-     conteiner.append(li);
+    conteiner.append(li);
 
-    if(option.time>0){
-      option.timer=setTimeout(_closePopup.bind(close), option.time);
+    if (option.time > 0) {
+      option.timer = setTimeout(_closePopup.bind(close), option.time);
     }
-    li.data('option',option)
+    li.data('option', option)
   }
 
   return {
@@ -278,22 +285,22 @@ var notification = (function() {
 })();
 
 
-$('[ref=popup]').on('click',function (e){
+$('[ref=popup]').on('click', function (e) {
   e.preventDefault();
-  $this=$(this);
-  el=$($this.attr('href'));
-  data=el.data();
+  $this = $(this);
+  el = $($this.attr('href'));
+  data = el.data();
 
-  data.question=el.html();
+  data.question = el.html();
   notification.alert(data);
 });
 
 
-$('.disabled').on('click',function (e){
+$('.disabled').on('click', function (e) {
   e.preventDefault();
-  $this=$(this);
-  data=$this.data();
-  if(data['button_yes'])data['buttonYes']=data['button_yes']
+  $this = $(this);
+  data = $this.data();
+  if (data['button_yes'])data['buttonYes'] = data['button_yes']
 
   notification.alert(data);
 });
