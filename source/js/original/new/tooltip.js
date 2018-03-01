a925051027da053217e9e5e291d51d6d84178ee2
Andrey <oxygenn@list.ru>
2018-03-01 18:37:23 +0300

    Стилизация тултипа

--- source/js/original/new/tooltip.js
+++ source/js/original/new/tooltip.js
@@ -1,9 +1,13 @@
 var myTooltip = function() {
     $('[data-toggle=tooltip]').tipso({
-        background: '#4A4A4A',
+        background: '#fff',
+        color: '#434a54',
         size: 'small',
-        delay: 100,
-        speed: 100,
+        delay: 10,
+        speed: 10,
+        width: 200,
+        //maxWidth: 258,
+        showArrow: true,
         onBeforeShow: function (ele, tipso) {
             this.content = ele.data('original-title');
         }
