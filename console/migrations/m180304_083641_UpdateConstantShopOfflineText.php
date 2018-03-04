748fd3499a6d5bbaf0b5bdd1185ea820794cdf37
Andrey <oxygenn@list.ru>
2018-03-04 12:38:30 +0300

    Изменена миграция. Текст Идёт на благотворительность

--- console/migrations/m180304_083641_UpdateConstantShopOfflineText.php
+++ console/migrations/m180304_083641_UpdateConstantShopOfflineText.php
@@ -16,7 +16,7 @@ class m180304_083641_UpdateConstantShopOfflineText extends Migration
         $this->execute('SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=\'TRADITIONAL,ALLOW_INVALID_DATES\';');
         $this->execute('SET SQL_MODE=\'ALLOW_INVALID_DATES\';');
         $constant = Constants::find()->where(['name' => 'shop_offline_text'])->one();
-        $constant->text =  '[{"goto":"\u0412\u0440\u0435\u043c\u0435\u043d\u043d\u043e \u043d\u0435\u0430\u043a\u0442\u0438\u0432\u0435\u043d","title_dop":"\u041c\u0430\u0433\u0430\u0437\u0438\u043d \u0432\u0440\u0435\u043c\u0435\u043d\u043d\u043e \u043d\u0435 \u0430\u043a\u0442\u0438\u0432\u0435\u043d","title_dop_blog":"\u041a\u044d\u0448\u0431\u044d\u043a \u043d\u0430\u043f\u0440\u0430\u0432\u043b\u044f\u0435\u0442\u0441\u044f \u043d\u0430 \u0431\u043b\u0430\u0433\u043e\u0442\u0432\u043e\u0440\u0438\u0442\u0435\u043b\u044c\u043d\u043e\u0441\u0442\u044c"}]';
+        $constant->text =  '[{"goto":"\u0412\u0440\u0435\u043c\u0435\u043d\u043d\u043e \u043d\u0435\u0430\u043a\u0442\u0438\u0432\u0435\u043d","title_dop":"\u041c\u0430\u0433\u0430\u0437\u0438\u043d \u0432\u0440\u0435\u043c\u0435\u043d\u043d\u043e \u043d\u0435 \u0430\u043a\u0442\u0438\u0432\u0435\u043d","title_dop_blog":"\u041A\u044D\u0448\u0431\u044D\u043A \u0438\u0434\u0435\u0442 \u043D\u0430 \u0431\u043B\u0430\u0433\u043E\u0442\u0432\u043E\u0440\u0438\u0442\u0435\u043B\u044C\u043D\u043E\u0441\u0442\u044C"}]';
         $constant->save();
 
         $constant = Constants::find()->where(['name' => 'shop_no_cashback'])->one();
