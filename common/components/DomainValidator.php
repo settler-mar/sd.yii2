4fb83d90f1f0128a5262868bfa50ffb2e9c8085c
Max Matiukhin <matuhinmax@mail.ru>
2018-03-19 09:18:22 +0200

    Заменил ф-ю тестирования домена. Для сафари новый фикс авторизации

--- common/components/DomainValidator.php
+++ common/components/DomainValidator.php
@@ -13,11 +13,15 @@ class DomainValidator extends Validator
     if (!isset($emailArray[1])) {
       $this->addError($model, $attribute, 'Email должен быть правильным адресом');
     }
+
     $domain = Domains::find()->where(['name' => $emailArray[1]])->one();
     if ($domain) {
       return null;
     }
-    if (!checkdnsrr( $emailArray[1] , "ANY")) {
+
+
+    //if (!checkdnsrr( $emailArray[1] , "ANY")) {
+    if (gethostbyname($emailArray[1])==$emailArray[1]) {
         $this->addError($model, $attribute, 'Необходим реально существующий адрес');
     }
 
