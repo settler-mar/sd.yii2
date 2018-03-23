48a6f6216bbf31d1e6a4401f24bd1443b0d42b00
Andrey <oxygenn@list.ru>
2018-03-23 21:12:56 +0300

    Скрипт #hashtags отработка get

--- source/js/original/new/hashtags.js
+++ source/js/original/new/hashtags.js
@@ -2,9 +2,14 @@ var hashTags = (function(){
 
     var hash = window.location.hash;
     if (hash != "") {
-        var links = $('a[href="'+hash+'"].modals_open');
-        if (links.length) {
-            $(links[0]).click();
+        var hashBody = hash.split("?");
+        if (hashBody[1]) {
+            window.location = location.origin + location.pathname + '?' + hashBody[1] + hashBody[0];
+        } else {
+            var links = $('a[href="' + hashBody[0] + '"].modals_open');
+            if (links.length) {
+                $(links[0]).click();
+            }
         }
     }
 
