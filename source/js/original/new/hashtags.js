6d0c0ef5b2050383758f6c187f11ea6092d919e6
Andrey <oxygenn@list.ru>
2018-03-26 10:10:48 +0300

    hashtags.js

--- source/js/original/new/hashtags.js
+++ source/js/original/new/hashtags.js
@@ -1,16 +1,25 @@
 var hashTags = (function(){
 
-    var hash = window.location.hash;
-    if (hash != "") {
-        var hashBody = hash.split("?");
-        if (hashBody[1]) {
-            window.location = location.origin + location.pathname + '?' + hashBody[1] + hashBody[0];
-        } else {
-            var links = $('a[href="' + hashBody[0] + '"].modals_open');
-            if (links.length) {
-                $(links[0]).click();
+    function locationHash() {
+        var hash = window.location.hash;
+
+        if (hash != "") {
+            var hashBody = hash.split("?");
+            if (hashBody[1]) {
+                window.location = location.origin + location.pathname + '?' + hashBody[1] + hashBody[0];
+            } else {
+                var links = $('a[href="' + hashBody[0] + '"].modals_open');
+                if (links.length) {
+                    $(links[0]).click();
+                }
             }
         }
     }
+
+    window.addEventListener("hashchange", function(){
+        locationHash();
+    });
+
+    locationHash()
 
 })();
