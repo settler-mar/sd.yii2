5b7f74fecfba5887103603f925eca984095d00fd
Andrey <oxygenn@list.ru>
2018-04-17 12:41:25 +0300

    На стартовой блок с установкой плагина

--- source/js/original/new/plugins.js
+++ source/js/original/new/plugins.js
@@ -1,23 +1,28 @@
 var plugins = (function(){
     var isOpera = navigator.userAgent.indexOf(' OPR/') >= 0;
+    var pluginInstallDivClass = 'install-plugin-index';
     var extensions = {
         'chrome': {
             'div_id': 'sd_chrome_app',
             'used': !!window.chrome && window.chrome.webstore !== null && !isOpera,
             'text':'Вы можете установить плагин для Хром бесплатно ....',
-            'href': 'https://chrome.google.com/webstore/category/extensions'
+            'href': 'https://chrome.google.com/webstore/category/extensions',
+            'install_button_class': 'plugin-browsers-link-chrome'
+
         },
         'firefox': {
             'div_id': 'sd_firefox_app',
             'used':  typeof InstallTrigger !== 'undefined',
             'text':'Вы можете установить плагин для Fairfox бесплатно ....',
-            'href': 'https://addons.mozilla.org/ru/firefox/'
+            'href': 'https://addons.mozilla.org/ru/firefox/',
+            'install_button_class': 'plugin-browsers-link-firefox'
         },
         'opera': {
             'div_id': 'sd_opera_app',
             'used': isOpera,
             'text':'Вы можете установить плагин для Opera бесплатно ....',
-            'href': 'https://addons.opera.com/ru/extensions/?ref=page'
+            'href': 'https://addons.opera.com/ru/extensions/?ref=page',
+            'install_button_class': 'plugin-browsers-link-opera'
         }
     };
 
@@ -42,6 +47,11 @@ var plugins = (function(){
         }
     }
 
+    function setButtonInstallVisible(buttonClass) {
+        $('.' + pluginInstallDivClass).removeClass('hidden');
+        $('.' + buttonClass).removeClass('hidden');
+    }
+
     function closeClick(){
         $('.install-plugin').addClass('install-plugin_hidden');
     }
@@ -53,6 +63,7 @@ var plugins = (function(){
                     var appId = document.querySelector('#'+extensions[key].div_id);
                     if (!appId) {
                         setPanel(extensions[key].text, extensions[key].href);
+                        setButtonInstallVisible(extensions[key].install_button_class);
                     }
                 }
             }
