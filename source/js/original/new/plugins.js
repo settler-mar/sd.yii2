7bf72d2ad8d6d6fd85e164ed34b3114d4d891ae8
Andrey <oxygenn@list.ru>
2018-04-18 16:20:55 +0300

    Панель установки плагина

--- source/js/original/new/plugins.js
+++ source/js/original/new/plugins.js
@@ -1,4 +1,7 @@
 var plugins = (function(){
+    var iconClose = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" width="12px" height="12px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357;" xml:space="preserve"><g>'+
+        '<g id="close"><polygon points="357,35.7 321.3,0 178.5,142.8 35.7,0 0,35.7 142.8,178.5 0,321.3 35.7,357 178.5,214.2 321.3,357 357,321.3     214.2,178.5   " fill="#FFFFFF"/>'+
+        '</svg>';
     var isOpera = navigator.userAgent.indexOf(' OPR/') >= 0;
     var pluginInstallDivClass = 'install-plugin-index';
     var pluginInstallDivAccountClass = 'install-plugin-account';
@@ -8,7 +11,7 @@ var plugins = (function(){
         'chrome': {
             'div_id': 'sd_chrome_app',
             'used': !!window.chrome && window.chrome.webstore !== null && !isOpera,
-            'text':'Вы можете установить плагин для Хром бесплатно ....',
+            'text':'Установите наше расширение для браузера и больше никогда не пропустите кэшбэк!',
             'href': 'https://chrome.google.com/webstore/category/extensions',
             'install_button_class': 'plugin-browsers-link-chrome'
 
@@ -16,14 +19,14 @@ var plugins = (function(){
         'firefox': {
             'div_id': 'sd_firefox_app',
             'used':  typeof InstallTrigger !== 'undefined',
-            'text':'Вы можете установить плагин для Fairfox бесплатно ....',
+            'text':'Установите наше расширение для браузера и больше никогда не пропустите кэшбэк!',
             'href': 'https://addons.mozilla.org/ru/firefox/',
             'install_button_class': 'plugin-browsers-link-firefox'
         },
         'opera': {
             'div_id': 'sd_opera_app',
             'used': isOpera,
-            'text':'Вы можете установить плагин для Opera бесплатно ....',
+            'text':'Установите наше расширение для браузера и больше никогда не пропустите кэшбэк!',
             'href': 'https://addons.opera.com/ru/extensions/?ref=page',
             'install_button_class': 'plugin-browsers-link-opera'
         }
@@ -35,8 +38,9 @@ var plugins = (function(){
         if (pluginInstallPanel && getCookie(cookiePanelHidden) !== '1' ) {
             //если выводить и в куки не выключили
             var closeDiv = document.createElement('a');
-            closeDiv.innerHTML = '&times;';
-            closeDiv.className = 'btn btn-mini btn-red btn-round install-plugin_close';
+            closeDiv.innerHTML = iconClose;
+            //closeDiv.className = 'btn btn-mini btn-red btn-round install-plugin_close';
+            closeDiv.className = 'install-plugin_button-close';
             closeDiv.onclick = closeClick;
             var textIn = '<div class="install-plugin_text">' + text + '</div>' +
                 '<a class="btn btn-mini btn-round"  href="' + href + '" target="_blank">Установить&nbsp;плагин</a>';
