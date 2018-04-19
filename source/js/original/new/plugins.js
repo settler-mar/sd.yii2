14392626b2741718f7557cf5578558ed0d14efa3
Andrey <oxygenn@list.ru>
2018-04-19 12:12:16 +0300

    Панель установки плагина в мобиле

--- source/js/original/new/plugins.js
+++ source/js/original/new/plugins.js
@@ -2,6 +2,13 @@ var plugins = (function(){
     var iconClose = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" width="12px" height="12px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357;" xml:space="preserve"><g>'+
         '<g id="close"><polygon points="357,35.7 321.3,0 178.5,142.8 35.7,0 0,35.7 142.8,178.5 0,321.3 35.7,357 178.5,214.2 321.3,357 357,321.3     214.2,178.5   " fill="#FFFFFF"/>'+
         '</svg>';
+    var template='<div class="page-wrap install-plugin_inner">'+
+                '<div class="install-plugin_text">{{text}}</div>'+
+                '<div class="install-plugin_buttons">'+
+                    '<a class="btn btn-mini btn-round install-plugin_button"  href="{{href}}" target="_blank">Установить&nbsp;плагин</a>'+
+                    '<div class="install-plugin_button-close">'+iconClose+'</div>'+
+                '</div>'+
+            '</div>';
     var isOpera = navigator.userAgent.indexOf(' OPR/') >= 0;
     var pluginInstallDivClass = 'install-plugin-index';
     var pluginInstallDivAccountClass = 'install-plugin-account';
@@ -36,24 +43,15 @@ var plugins = (function(){
     function setPanel(text, href) {
         var pluginInstallPanel = document.querySelector('#plugin-install-panel');//выводить ли панель
         if (pluginInstallPanel && getCookie(cookiePanelHidden) !== '1' ) {
-            //если выводить и в куки не выключили
-            var closeDiv = document.createElement('a');
-            closeDiv.innerHTML = iconClose;
-            //closeDiv.className = 'btn btn-mini btn-red btn-round install-plugin_close';
-            closeDiv.className = 'install-plugin_button-close';
-            closeDiv.onclick = closeClick;
-            var textIn = '<div class="install-plugin_text">' + text + '</div>' +
-                '<a class="btn btn-mini btn-round"  href="' + href + '" target="_blank">Установить&nbsp;плагин</a>';
-            var sectionInner = document.createElement('div');
-            sectionInner.className = 'page-wrap install-plugin_inner';
-            sectionInner.innerHTML = textIn;
-            sectionInner.appendChild(closeDiv);
+            template = template.replace('{{text}}', text);
+            template = template.replace('{{href}}', href);
             var section = document.createElement('section');
             section.className = 'install-plugin';
-            section.appendChild(sectionInner);
+            section.innerHTML = template;
             var contentWrap = document.body.querySelector('.content-wrap');
             if (contentWrap) {
                 contentWrap.insertBefore(section, contentWrap.firstChild);
+                document.querySelector('.install-plugin_button-close').onclick = closeClick;
             }
         }
     }
