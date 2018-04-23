59eb8e538312deee9e944f3cc8a5ab8c6c557c4e
Andrey <oxygenn@list.ru>
2018-04-23 23:58:44 +0300

    Логика показа точек продаж

--- source/js/original/new/store_points.js
+++ source/js/original/new/store_points.js
@@ -3,26 +3,28 @@ var store_points = (function(){
 
     function changeCountry(){
         var that = $('#store_point_country');
-        var selectOptions = $(that).find('option');
-        var data = $('option:selected', that).data('cities'),
-            points= $('#store-points'),
-            country = $('option:selected', that).attr('value');
-        if (selectOptions.length > 1 && data) {
-            data = data.split(',');
-            if (data.length > 0) {
-                var select = document.getElementById('store_point_city');
-                //var options = '<option value="">Выберите город</option>';
-                var options = '';
-                data.forEach(function (item) {
-                    options += '<option value="' + item + '">' + item + '</option>';
-                });
-                select.innerHTML = options;
+        if (that.length) {
+            var selectOptions = $(that).find('option');
+            var data = $('option:selected', that).data('cities'),
+                points = $('#store-points'),
+                country = $('option:selected', that).attr('value');
+            if (selectOptions.length > 1 && data) {
+                data = data.split(',');
+                if (data.length > 0) {
+                    var select = document.getElementById('store_point_city');
+                    //var options = '<option value="">Выберите город</option>';
+                    var options = '';
+                    data.forEach(function (item) {
+                        options += '<option value="' + item + '">' + item + '</option>';
+                    });
+                    select.innerHTML = options;
+                }
             }
+            //$(points).addClass('hidden');
+            // googleMap.showMap();
+            // googleMap.showMarker(country, '');
+            changeCity();
         }
-        $(points).addClass('hidden');
-       // googleMap.showMap();
-       // googleMap.showMarker(country, '');
-        changeCity();
 
     }
 
@@ -32,36 +34,38 @@ var store_points = (function(){
         }
 
         var that = $('#store_point_city');
-        var city = $('option:selected', that).attr('value'),
-            country = $('option:selected', $('#store_point_country')).attr('value'),
-            points= $('#store-points');
-        if (country && city) {
-            var items = points.find('.store-points__points_row'),
-                visible = false;
-            try {
-                googleMap.showMarker(country, city);
-            } catch (err) {
-                console.log(err);
-            }
-            $.each(items, function(index, div){
-                if ($(div).data('city') == city && $(div).data('country') == country){
-                    $(div).removeClass('store-points__points_row-hidden');
-                    visible = true;
-                } else {
-                    $(div).addClass('store-points__points_row-hidden') ;
+        if (that.length) {
+            var city = $('option:selected', that).attr('value'),
+                country = $('option:selected', $('#store_point_country')).attr('value'),
+                points = $('#store-points');
+            if (country && city) {
+                var items = points.find('.store-points__points_row'),
+                    visible = false;
+                try {
+                    googleMap.showMarker(country, city);
+                } catch (err) {
+                    console.log(err);
                 }
-            });
-            if (visible) {
-                $(points).removeClass('store-points__points-hidden');
-                googleMap.showMap();
+                $.each(items, function (index, div) {
+                    if ($(div).data('city') == city && $(div).data('country') == country) {
+                        $(div).removeClass('store-points__points_row-hidden');
+                        visible = true;
+                    } else {
+                        $(div).addClass('store-points__points_row-hidden');
+                    }
+                });
+                if (visible) {
+                    $(points).removeClass('store-points__points-hidden');
+                    googleMap.showMap();
 
+                } else {
+                    $(points).addClass('store-points__points-hidden');
+                    googleMap.hideMap();
+                }
             } else {
                 $(points).addClass('store-points__points-hidden');
                 googleMap.hideMap();
             }
-        } else {
-            $(points).addClass('store-points__points-hidden');
-            googleMap.hideMap();
         }
     }
 
@@ -78,11 +82,8 @@ var store_points = (function(){
 
     });
 
-    function load(){
-        changeCountry();
-    }
+    changeCountry();
 
-    load();
 
 })();
 
