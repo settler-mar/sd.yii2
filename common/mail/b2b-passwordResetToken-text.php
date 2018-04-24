f42ec20af8660959c2dc317ba0d676fb214bf037
Andrey <oxygenn@list.ru>
2018-04-24 21:21:53 +0300

    Поправлен шаблон письма сброса пароля b2b

--- common/mail/b2b-passwordResetToken-text.php
+++ common/mail/b2b-passwordResetToken-text.php
@@ -3,7 +3,7 @@
 /* @var $this yii\web\View */
 /* @var $user common\models\User */
 
-$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['users/default/reset', 'token' => $user->password_reset_token, 'password'=>$password]);
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['users/default/reset', 'token' => $user->password_reset_token]);
 ?>
 Здраствуйте, <?= $user->fio ?>,
 
