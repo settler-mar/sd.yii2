4a30be1d5d7ef33786bc9bd47eccebb375cb150c
Max Matiukhin <matuhinmax@mail.ru>
2018-04-25 13:18:50 +0300

    Стилизация placeholder

--- source/js/original/new/placeholder.js
+++ source/js/original/new/placeholder.js
@@ -1,12 +1,18 @@
 var placeholder = (function(){
-  function run() {
-    var els=$('[placeholder]');
+  function run(par) {
+    var els;
+    if(!par)
+      els=$('[placeholder]');
+    else
+      els=$(par).find('[placeholder]');
+
     for(var i = 0; i<els.length;i++){
       var el=els.eq(i);
       var text = el.attr('placeholder');
       el.attr('placeholder','');
       if(text.length<2)continue;
 
+      var inputValue = el.val();
       var el_id = el.attr('id');
       if(!el_id){
         el_id='el_forms_'+Math.round(Math.random()*10000);
@@ -14,7 +20,7 @@ var placeholder = (function(){
       }
 
       var div = $('<label/>',{
-        'class':'placeholder',
+        'class':'placeholder'+(inputValue.length>0?' focused':''),
         'text': text,
         'for':el_id
       });
@@ -22,13 +28,13 @@ var placeholder = (function(){
       el.before(div);
     }
     els.focus(function(){
-      $(this).prev('label').addClass('focused');
+      $(this).prev('label.placeholder').addClass('focused');
     });
     els.blur(function(){
       var inputValue = $(this).val();
       if ( inputValue == "" ) {
         $(this).removeClass('filled');
-        $(this).prev('label').removeClass('focused');
+        $(this).prev('label.placeholder').removeClass('focused');
       } else {
         $(this).addClass('filled');
       }
