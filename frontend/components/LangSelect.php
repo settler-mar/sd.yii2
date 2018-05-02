f5f3e8f7e482a603e3007e28b69bd2899b168574
Max Matiukhin <matuhinmax@mail.ru>
2018-05-02 10:11:04 +0300

    Добавил болванку виджета и список языков в конфиге

---
+++ frontend/components/LangSelect.php
@@ -0,0 +1,17 @@
+<?php
+namespace frontend\components;
+
+use yii\base\Widget;
+
+class LangSelect extends Widget
+{
+  public function init()
+  {
+    parent::init();
+  }
+
+  public function run()
+  {
+    return "test";
+  }
+}
