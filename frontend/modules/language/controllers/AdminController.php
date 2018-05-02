5e6575b7bad072611e0624f2053ddfbde7d77cc0
Max Matiukhin <matuhinmax@mail.ru>
2018-05-02 14:52:24 +0300

    Настроил вывод теста языка

--- frontend/modules/language/controllers/AdminController.php
+++ frontend/modules/language/controllers/AdminController.php
@@ -32,8 +32,9 @@ class AdminController extends Controller
         'total'=>['WARNING'=>0,'ERROR'=>0,'NOTICE'=>0]
       ];
 
+      $data['lg']=array();
       foreach ($lg_list as $lg_k => $lg){
-        $data[$lg_k]=array('total'=>['WARNING'=>0,'ERROR'=>0,'NOTICE'=>0]);
+        $data['lg'][$lg_k]=array('total'=>['WARNING'=>0,'ERROR'=>0,'NOTICE'=>0]);
       }
 
       $bp=Yii::$app->basePath.'/language/';
@@ -46,36 +47,36 @@ class AdminController extends Controller
         $base=require($bp.$base_lang.'/'.$file);
         foreach ($lg_list as $lg_k => $lg){
           $path=$bp.$lg_k.'/'.$file;
-          $data[$lg_k][$file]=['WARNING'=>array(),'ERROR'=>array(),'NOTICE'=>array(),'TYPE'=>'file_php','PATH'=>$path];
+          $data['lg'][$lg_k][$file]=['WARNING'=>array(),'ERROR'=>array(),'NOTICE'=>array(),'TYPE'=>'file_php','PATH'=>$path];
           if(!is_readable($path)){
-            $data[$lg_k][$file]['WARNING'][]='Фаил отутствует';
+            $data['lg'][$lg_k][$file]['WARNING'][]='Фаил отутствует';
             continue;
           }
           $transl=require($path);
 
           foreach ($base as $k => $item){
             if(!isset($transl[$k])){
-              $data[$lg_k][$file]['WARNING'][]='Параметр <b>'.$k.'</b> отутствует';
+              $data['lg'][$lg_k][$file]['WARNING'][]='Параметр <b>'.$k.'</b> отутствует';
               continue;
             }
 
             if(trim($transl[$k])==$item){
-              $data[$lg_k][$file]['NOTICE'][]='Параметр <b>'.$k.'</b> совпадает с оригинальным языком';
+              $data['lg'][$lg_k][$file]['NOTICE'][]='Параметр <b>'.$k.'</b> совпадает с оригинальным языком';
               continue;
             }
 
             if(strlen(trim($transl[$k]))<2){
-              $data[$lg_k][$file]['ERROR'][]='Параметр <b>'.$k.'</b> не заполнен';
+              $data['lg'][$lg_k][$file]['ERROR'][]='Параметр <b>'.$k.'</b> не заполнен';
               continue;
             }
           }
         }
-        $data[$lg_k]['total']['WARNING']+=count($data[$lg_k][$file]['WARNING']);
-        $data[$lg_k]['total']['NOTICE']+=count($data[$lg_k][$file]['NOTICE']);
-        $data[$lg_k]['total']['ERROR']+=count($data[$lg_k][$file]['ERROR']);
-        $data['total']['WARNING']+=count($data[$lg_k][$file]['WARNING']);
-        $data['total']['NOTICE']+=count($data[$lg_k][$file]['NOTICE']);
-        $data['total']['ERROR']+=count($data[$lg_k][$file]['ERROR']);
+        $data['lg'][$lg_k]['total']['WARNING']+=count($data['lg'][$lg_k][$file]['WARNING']);
+        $data['lg'][$lg_k]['total']['NOTICE']+=count($data['lg'][$lg_k][$file]['NOTICE']);
+        $data['lg'][$lg_k]['total']['ERROR']+=count($data['lg'][$lg_k][$file]['ERROR']);
+        $data['total']['WARNING']+=count($data['lg'][$lg_k][$file]['WARNING']);
+        $data['total']['NOTICE']+=count($data['lg'][$lg_k][$file]['NOTICE']);
+        $data['total']['ERROR']+=count($data['lg'][$lg_k][$file]['ERROR']);
 
       }
 
@@ -84,37 +85,38 @@ class AdminController extends Controller
       $file='json';
       foreach ($lg_list as $lg_k => $lg){
         $path=$bp.$lg_k.'.json';
-        $data[$lg_k][$file]=['WARNING'=>array(),'ERROR'=>array(),'NOTICE'=>array(),'TYPE'=>'file_json','PATH'=>$path];
+        $data['lg'][$lg_k][$file]=['WARNING'=>array(),'ERROR'=>array(),'NOTICE'=>array(),'TYPE'=>'file_json','PATH'=>$path];
         if(!is_readable($path)){
-          $data[$lg_k][$file]['WARNING'][]='Фаил отутствует';
+          $data['lg'][$lg_k][$file]['WARNING'][]='Фаил отутствует';
           continue;
         }
         $transl=json_decode(file_get_contents($path),true);
 
         foreach ($base as $k => $item){
           if(!isset($transl[$k])){
-            $data[$lg_k][$file]['WARNING'][]='Параметр <b>'.$k.'</b> отутствует';
+            $data['lg'][$lg_k][$file]['WARNING'][]='Параметр <b>'.$k.'</b> отутствует';
             continue;
           }
 
           if(trim($transl[$k])==$item){
-            $data[$lg_k][$file]['NOTICE'][]='Параметр <b>'.$k.'</b> совпадает с оригинальным языком';
+            $data['lg'][$lg_k][$file]['NOTICE'][]='Параметр <b>'.$k.'</b> совпадает с оригинальным языком';
             continue;
           }
 
           if(strlen(trim($transl[$k]))<2){
-            $data[$lg_k][$file]['ERROR'][]='Параметр <b>'.$k.'</b> не заполнен';
+            $data['lg'][$lg_k][$file]['ERROR'][]='Параметр <b>'.$k.'</b> не заполнен';
             continue;
           }
         }
       }
-      $data[$lg_k]['total']['WARNING']+=count($data[$lg_k][$file]['WARNING']);
-      $data[$lg_k]['total']['NOTICE']+=count($data[$lg_k][$file]['NOTICE']);
-      $data[$lg_k]['total']['ERROR']+=count($data[$lg_k][$file]['ERROR']);
-      $data['total']['WARNING']+=count($data[$lg_k][$file]['WARNING']);
-      $data['total']['NOTICE']+=count($data[$lg_k][$file]['NOTICE']);
-      $data['total']['ERROR']+=count($data[$lg_k][$file]['ERROR']);
+      $data['lg'][$lg_k]['total']['WARNING']+=count($data['lg'][$lg_k][$file]['WARNING']);
+      $data['lg'][$lg_k]['total']['NOTICE']+=count($data['lg'][$lg_k][$file]['NOTICE']);
+      $data['lg'][$lg_k]['total']['ERROR']+=count($data['lg'][$lg_k][$file]['ERROR']);
+      $data['total']['WARNING']+=count($data['lg'][$lg_k][$file]['WARNING']);
+      $data['total']['NOTICE']+=count($data['lg'][$lg_k][$file]['NOTICE']);
+      $data['total']['ERROR']+=count($data['lg'][$lg_k][$file]['ERROR']);
 
+      //ddd($data);
       return $this->render('index',$data);
     }
 }
