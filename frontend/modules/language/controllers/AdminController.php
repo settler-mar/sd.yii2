204e2c328217bb8a1e94596423e998c1872e79a8
Andrey <oxygenn@list.ru>
2018-05-03 17:31:54 +0300

    Тест для lg_meta

--- frontend/modules/language/controllers/AdminController.php
+++ frontend/modules/language/controllers/AdminController.php
@@ -4,6 +4,9 @@ namespace frontend\modules\language\controllers;
 
 use yii\web\Controller;
 use Yii;
+use frontend\modules\meta\models\Meta;
+use frontend\modules\meta\models\LgMeta;
+
 
 class AdminController extends Controller
 {
@@ -116,6 +119,18 @@ class AdminController extends Controller
       $data['total']['NOTICE']+=count($data['lg'][$lg_k][$file]['NOTICE']);
       $data['total']['ERROR']+=count($data['lg'][$lg_k][$file]['ERROR']);
 
+      //meta
+      foreach ($lg_list as $lg_k => $lg){
+          $data['lg'][$lg_k]['meta'] = ['WARNING'=>[]];
+          $metas = Meta::find()->all();
+          foreach ($metas as $meta) {
+              $lang = LgMeta::find()->where(['meta_id' => $meta->uid, 'language' => $lg_k])->one();
+              if (!$lang) {
+                  $data['lg'][$lg_k]['meta']['WARNING'][] = $meta->page;
+              }
+          }
+      }
+      $data['lg'][$lg_k]['total']['WARNING']+=count($data['lg'][$lg_k]['meta']['WARNING']);
       //ddd($data);
       return $this->render('index',$data);
     }
