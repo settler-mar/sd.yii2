2cf4c5a0a0010bfaf1892faed595d8e928b8ae36
Max Matiukhin <matuhinmax@mail.ru>
2018-05-07 14:01:15 +0300

    Для логирования добавил запись идентификатора пользователя

--- console/migrations/m180507_140000_active_record_change_log.php
+++ console/migrations/m180507_140000_active_record_change_log.php
@@ -2,11 +2,17 @@
 /**
  * @author Pan Wenbin <panwenbin@gmail.com>
  */
+use yii\db\Migration;
 
-class m170819_140000_active_record_change_log extends \yii\db\Migration
+class m180507_140000_active_record_change_log extends Migration
 {
     public function up()
     {
+      $this->execute('SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=\'TRADITIONAL,ALLOW_INVALID_DATES\';');
+      $this->execute('SET SQL_MODE=\'ALLOW_INVALID_DATES\';');
+
+      $this->execute('DROP TABLE IF EXISTS `active_record_change_log`');
+
         $this->createTable('{{%active_record_change_log}}', [
             'id' => $this->primaryKey(),
             'event' => $this->string()->comment('Event Name'),
@@ -16,8 +22,16 @@ class m170819_140000_active_record_change_log extends \yii\db\Migration
             'old_attributes' => $this->text()->comment('Old Attributes Json'),
             'new_attributes' => $this->text()->comment('New Attributes Json'),
             'log_at' => $this->integer(),
-        ]);
+            'user_id' => $this->integer(),
+        ], 'CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ENGINE=InnoDB');
         $this->createIndex('model', '{{%active_record_change_log}}', ['model']);
+        $this->addForeignKey (
+            'fk_ar_log_user_id',
+            '{{%active_record_change_log}}',
+            'user_id',
+            'cw_users',
+            'uid'
+        );
     }
 
     public function down()
