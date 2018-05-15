fc254da2b6d8320823d0af4800120cb0a1fc1ad4
Andrey <oxygenn@list.ru>
2018-05-15 20:09:45 +0300

    Аккордеон больше-меньше для контента

--- frontend/components/SdController.php
+++ frontend/components/SdController.php
@@ -235,7 +235,9 @@ class SdController extends Controller
         if (isset($this->params['breadcrumbs'][intval(count($this->params['breadcrumbs'])) - 1]['url'])) {
             $this->params['breadcrumbs'][intval(count($this->params['breadcrumbs'])) - 1]['url'] = null;
         }
-        $params['pagination_page'] = $params['page'];
+        if (isset($params['page'])) {
+            $params['pagination_page'] = $params['page'];
+        }
 
         return parent::render($view, $params);
     }
