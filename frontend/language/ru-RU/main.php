cb3ff7193152fc5fe9122136728caa20d9482158
Andrey <oxygenn@list.ru>
2018-06-05 17:27:42 +0300

    Поправка в переводе

--- frontend/language/ru-RU/main.php
+++ frontend/language/ru-RU/main.php
@@ -108,7 +108,7 @@ return [
     'breadcrumbs_search_abc' => 'Алфавитный поиск',
     'breadcrumbs_page' => 'Страница',
     'breadcrumbs_stores_online' => 'Оффлайн-магазины',
-    'breadcrumbs_stores_offine' => 'Магазины',
+    'breadcrumbs_stores_offline' => 'Магазины',
     'breadcrumbs_stores_favorite' => 'Мои избранные',
     'coupons_search_h1' => 'Промокоды. Результат поиска по запросу',
     'stores_abc_default_h1_{count}_' => 'Магазины - алфавитный поиск <span>({count})</span>',
