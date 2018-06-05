6e2709397b157ba3a7e7dc5a3b3865b1ae0fbcd3
Andrey <oxygenn@list.ru>
2018-06-05 12:30:49 +0300

    Миграция. Изменены страницы mail-ru-level

---
+++ console/migrations/m180605_083712_updateMetaPagesMailRu.php
@@ -0,0 +1,120 @@
+<?php
+
+use yii\db\Migration;
+use frontend\modules\meta\models\meta;
+
+/**
+ * Class m180605_083712_updateMetaPagesMailRu
+ */
+class m180605_083712_updateMetaPagesMailRu extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute('SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=\'TRADITIONAL,ALLOW_INVALID_DATES\';');
+        $this->execute('SET SQL_MODE=\'ALLOW_INVALID_DATES\';');
+
+        $sql = 'UPDATE `cw_metadata` SET `content`="<div class=\"mail-ru\">
+<p class=\"clearfix\"><img style=\"float: right;\" src=\"https://secretdiscounter.ru/img/bonus.mail.ru/secretdiscounter-mailbonus-30.png\" alt=\"secretdiscounter-mailbonus-30.png (38 KB)\" width=\"197\" height=\"100\" /><strong>Кэшбэк-сервис SecretDiscounter возвращает деньги с покупок в 1400+ магазинов</strong>. <strong>В онлайне и оффлайне</strong>. А еще у нас существует <strong><a href=\"https://secretdiscounter.ru/loyalty\" target=\"_blank\" rel=\"noopener\">четыре уровня лояльности</a></strong>, и платиновый аккаунт (Platinum) <strong>увеличивает ваш кэшбэк на 30%</strong>. Это означает, что если стандартная ставка в каком-то магазине составляет 10% с каждого заказа, то с platinum-аккаунтом вы будете возвращать уже 13%. И сегодня SecretDiscounter и Почта Mail.ru <strong>дарят вам такой аккаунт совершенно бесплатно</strong>!</p>
+{% if user_id == \'0\' %}
+<p>&nbsp;</p>
+<div class=\"new-year_text align-center\">
+<h2 class=\"new-year_item-description-header title-no-line\">Чтобы получить платиновый аккаунт, расскажите о нашей акции друзьям:</h2>
+<div><a class=\"btn\" href=\"#registration\">Зарегистрироваться</a></div>
+<div style=\"margin:15px;font-weight:bold;\" class=\"new-year_item-description-header\">Также мы будем признательны, если вы расскажете о нашей акции друзьям</div>
+{{ _include(\"share\",{\'promo_code\':\'silver0418\', \'ref_link\':\'0\'})|raw }}&nbsp;
+</div>
+{% endif %}</div>" WHERE `page` = "mail-ru-level-1"';
+        $this->execute($sql);
+
+        $sql = 'UPDATE `cw_metadata` SET `content`="<div class=\"mail-ru\">
+<p class=\"clearfix\"><img style=\"float: right;\" src=\"https://secretdiscounter.ru/img/bonus.mail.ru/secretdiscounter-mailbonus-30.png\" alt=\"secretdiscounter-mailbonus-30.png (38 KB)\" width=\"197\" height=\"100\" /><strong>Кэшбэк-сервис SecretDiscounter возвращает деньги с покупок в 1400+ магазинов</strong>. <strong>В онлайне и оффлайне</strong>. А еще у нас существует <strong><a href=\"https://secretdiscounter.ru/loyalty\" target=\"_blank\" rel=\"noopener\">четыре уровня лояльности</a></strong>, и платиновый аккаунт (Platinum) <strong>увеличивает ваш кэшбэк на 30%</strong>. Это означает, что если стандартная ставка в каком-то магазине составляет 10% с каждого заказа, то с platinum-аккаунтом вы будете возвращать уже 13%. И сегодня SecretDiscounter и Почта Mail.ru <strong>дарят вам такой аккаунт совершенно бесплатно</strong>!</p>
+{% if user_id == \'0\' %}
+<p>&nbsp;</p>
+<div class=\"new-year_text align-center\">
+<h2 class=\"new-year_item-description-header title-no-line\">Чтобы получить платиновый аккаунт, расскажите о нашей акции друзьям:</h2>
+<div><a class=\"btn\" href=\"#registration\">Зарегистрироваться</a></div>
+<div style=\"margin:15px;font-weight:bold;\" class=\"new-year_item-description-header\">Также мы будем признательны, если вы расскажете о нашей акции друзьям</div>
+{{ _include(\"share\",{\'promo_code\':\'gold0418\', \'ref_link\':\'0\'})|raw }}&nbsp;
+</div>
+{% endif %}</div>" WHERE `page` = "mail-ru-level-2"';
+        $this->execute($sql);
+
+        $sql = 'UPDATE `cw_metadata` SET `content`="<div class=\"mail-ru\">
+<p class=\"clearfix\"><img style=\"float: right;\" src=\"https://secretdiscounter.ru/img/bonus.mail.ru/secretdiscounter-mailbonus-30.png\" alt=\"secretdiscounter-mailbonus-30.png (38 KB)\" width=\"197\" height=\"100\" /><strong>Кэшбэк-сервис SecretDiscounter возвращает деньги с покупок в 1400+ магазинов</strong>. <strong>В онлайне и оффлайне</strong>. А еще у нас существует <strong><a href=\"https://secretdiscounter.ru/loyalty\" target=\"_blank\" rel=\"noopener\">четыре уровня лояльности</a></strong>, и платиновый аккаунт (Platinum) <strong>увеличивает ваш кэшбэк на 30%</strong>. Это означает, что если стандартная ставка в каком-то магазине составляет 10% с каждого заказа, то с platinum-аккаунтом вы будете возвращать уже 13%. И сегодня SecretDiscounter и Почта Mail.ru <strong>дарят вам такой аккаунт совершенно бесплатно</strong>!</p>
+{% if user_id == \'0\' %}
+<p>&nbsp;</p>
+<div class=\"new-year_text align-center\">
+<h2 class=\"new-year_item-description-header title-no-line\">Чтобы получить платиновый аккаунт, расскажите о нашей акции друзьям:</h2>
+<div><a class=\"btn\" href=\"#registration\">Зарегистрироваться</a></div>
+<div style=\"margin:15px;font-weight:bold;\" class=\"new-year_item-description-header\">Также мы будем признательны, если вы расскажете о нашей акции друзьям</div>
+{{ _include(\"share\",{\'promo_code\':\'platinum0418\', \'ref_link\':\'0\'})|raw }}&nbsp;
+</div>
+{% endif %}</div>" WHERE `page` = "mail-ru-level-3"';
+        $this->execute($sql);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->execute('SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=\'TRADITIONAL,ALLOW_INVALID_DATES\';');
+        $this->execute('SET SQL_MODE=\'ALLOW_INVALID_DATES\';');
+        $sql = 'UPDATE `cw_metadata` SET `content`="<div class=\"mail-ru\">
+<p class=\"clearfix\"><img style=\"float: right;\" src=\"https://secretdiscounter.ru/img/bonus.mail.ru/secretdiscounter-mailbonus-30.png\" alt=\"secretdiscounter-mailbonus-30.png (38 KB)\" width=\"197\" height=\"100\" /><strong>Кэшбэк-сервис SecretDiscounter возвращает деньги с покупок в 1400+ магазинов</strong>. <strong>В онлайне и оффлайне</strong>. А еще у нас существует <strong><a href=\"https://secretdiscounter.ru/loyalty\" target=\"_blank\" rel=\"noopener\">четыре уровня лояльности</a></strong>, и платиновый аккаунт (Platinum) <strong>увеличивает ваш кэшбэк на 30%</strong>. Это означает, что если стандартная ставка в каком-то магазине составляет 10% с каждого заказа, то с platinum-аккаунтом вы будете возвращать уже 13%. И сегодня SecretDiscounter и Почта Mail.ru <strong>дарят вам такой аккаунт совершенно бесплатно</strong>!</p>
+{% if user_id == \'0\' %}
+<p>&nbsp;</p>
+<div class=\"new-year_text align-center\">
+<h2 class=\"new-year_item-description-header title-no-line\">Чтобы получить платиновый аккаунт, расскажите о нашей акции друзьям:</h2>
+{{ _include(\"share\",{\'promo_code\':\'silver0418\', \'ref_link\':\'0\'})|raw }}&nbsp;
+<p style=\"font-size: 14px; line-height: 1.1em; margin-top: 7px;\">ссылка для регистрации премиум-аккаунта появится после того,<br />как вы расскажете друзьям&nbsp;</p>
+<div class=\"on_promo\"><a class=\"registration btn\" href=\"#registration\">Зарегистрировать Platinum-аккаунт</a></div>
+</div>
+{% endif %}</div>" WHERE `page` = "mail-ru-level-1"';
+        $this->execute($sql);
+
+        $sql = 'UPDATE `cw_metadata` SET `content`="<div class=\"mail-ru\">
+<p class=\"clearfix\"><img style=\"float: right;\" src=\"https://secretdiscounter.ru/img/bonus.mail.ru/secretdiscounter-mailbonus-30.png\" alt=\"secretdiscounter-mailbonus-30.png (38 KB)\" width=\"197\" height=\"100\" /><strong>Кэшбэк-сервис SecretDiscounter возвращает деньги с покупок в 1400+ магазинов</strong>. <strong>В онлайне и оффлайне</strong>. А еще у нас существует <strong><a href=\"https://secretdiscounter.ru/loyalty\" target=\"_blank\" rel=\"noopener\">четыре уровня лояльности</a></strong>, и платиновый аккаунт (Platinum) <strong>увеличивает ваш кэшбэк на 30%</strong>. Это означает, что если стандартная ставка в каком-то магазине составляет 10% с каждого заказа, то с platinum-аккаунтом вы будете возвращать уже 13%. И сегодня SecretDiscounter и Почта Mail.ru <strong>дарят вам такой аккаунт совершенно бесплатно</strong>!</p>
+{% if user_id == \'0\' %}
+<p>&nbsp;</p>
+<div class=\"new-year_text align-center\">
+<h2 class=\"new-year_item-description-header title-no-line\">Чтобы получить платиновый аккаунт, расскажите о нашей акции друзьям:</h2>
+{{ _include(\"share\",{\'promo_code\':\'gold0418\', \'ref_link\':\'0\'})|raw }}&nbsp;
+<p style=\"font-size: 14px; line-height: 1.1em; margin-top: 7px;\">ссылка для регистрации премиум-аккаунта появится после того,<br />как вы расскажете друзьям&nbsp;</p>
+<div class=\"on_promo\"><a class=\"registration btn\" href=\"#registration\">Зарегистрировать Platinum-аккаунт</a></div>
+</div>
+{% endif %}</div>" WHERE `page` = "mail-ru-level-2"';
+        $this->execute($sql);
+
+        $sql = 'UPDATE `cw_metadata` SET `content`="<div class=\"mail-ru\">
+<p class=\"clearfix\"><img style=\"float: right;\" src=\"https://secretdiscounter.ru/img/bonus.mail.ru/secretdiscounter-mailbonus-30.png\" alt=\"secretdiscounter-mailbonus-30.png (38 KB)\" width=\"197\" height=\"100\" /><strong>Кэшбэк-сервис SecretDiscounter возвращает деньги с покупок в 1400+ магазинов</strong>. <strong>В онлайне и оффлайне</strong>. А еще у нас существует <strong><a href=\"https://secretdiscounter.ru/loyalty\" target=\"_blank\" rel=\"noopener\">четыре уровня лояльности</a></strong>, и платиновый аккаунт (Platinum) <strong>увеличивает ваш кэшбэк на 30%</strong>. Это означает, что если стандартная ставка в каком-то магазине составляет 10% с каждого заказа, то с platinum-аккаунтом вы будете возвращать уже 13%. И сегодня SecretDiscounter и Почта Mail.ru <strong>дарят вам такой аккаунт совершенно бесплатно</strong>!</p>
+{% if user_id == \'0\' %}
+<p>&nbsp;</p>
+<div class=\"new-year_text align-center\">
+<h2 class=\"new-year_item-description-header title-no-line\">Чтобы получить платиновый аккаунт, расскажите о нашей акции друзьям:</h2>
+{{ _include(\"share\",{\'promo_code\':\'platinum0418\', \'ref_link\':\'0\'})|raw }}&nbsp;
+<p style=\"font-size: 14px; line-height: 1.1em; margin-top: 7px;\">ссылка для регистрации премиум-аккаунта появится после того,<br />как вы расскажете друзьям&nbsp;</p>
+<div class=\"on_promo\"><a class=\"registration btn\" href=\"#registration\">Зарегистрировать Platinum-аккаунт</a></div>
+</div>
+{% endif %}</div>" WHERE `page` = "mail-ru-level-3"';
+        $this->execute($sql);
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180605_083712_updateMetaPagesMailRu cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
