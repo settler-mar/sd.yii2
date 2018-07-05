2aefe516403aba5282d9e0b03a65971474acb67b
Andrey <oxygenn@list.ru>
2018-07-05 15:48:57 +0300

    Баннер на страницу шопа

--- frontend/modules/banners/models/Banners.php
+++ frontend/modules/banners/models/Banners.php
@@ -42,6 +42,7 @@ class Banners extends \yii\db\ActiveRecord
         'shops-catalog-left-menu' => ['name' => 'Шопы. Левое меню. Основной каталог'],
         'coupons-left-menu' => ['name' => 'Купоны. Левое меню'],
         'coupons-catalog-left-menu' => ['name' => 'Купоны. Левое меню. Основной каталог'],
+        'shop-page' => ['name' => 'В магазине'],
     ];
 
     /**
@@ -221,11 +222,14 @@ class Banners extends \yii\db\ActiveRecord
     {
         //
         $place = !empty($params['place']) ? $params['place'] : false;
-        if (is_string($place))
+        if (is_string($place)) {
             $place = explode(',', $place);
+        }
 
         if (is_array($place)) {
-            foreach ($place as &$item) $item = trim($item);
+            foreach ($place as &$item) {
+                $item = trim($item);
+            }
         }
         $language =  Yii::$app->language;
         $region = Yii::$app->params['region'];
@@ -241,7 +245,7 @@ class Banners extends \yii\db\ActiveRecord
             $cacheName,
             function () use ($place, $language, $region) {
                 $banners = self::find()
-                    ->select(['picture', 'url', 'new_window', 'show_desctop', 'show_mobile']);
+                    ->select(['uid', 'picture', 'url', 'new_window', 'show_desctop', 'show_mobile']);
                 if ($place) {
                     foreach ($place as $item) {
                         $banners->orWhere(['like', 'places', $item]);
@@ -271,6 +275,23 @@ class Banners extends \yii\db\ActiveRecord
             $dependency
         );
         if ($banners) {
+            if (!empty($params['options']['random'])) {
+                //случайно один баннер из не просмотренных
+                $bannerLastList =  Yii::$app->session->get('sd_banner_last_list');
+                $bannerLastList = $bannerLastList && count($bannerLastList) < count($banners) ? $bannerLastList : [];
+                if (!empty($bannerLastList)) {
+                    foreach ($banners as $key => $banner) {
+                        if (in_array($banner['uid'], $bannerLastList)) {
+                            unset($banners[$key]);
+                        }
+                    }
+                    $banners = array_values($banners);
+                }
+                $index = mt_rand(0, count($banners)-1);
+                $banners = [$banners[$index]];
+                $bannerLastList[] = $banners[0]['uid'];
+                Yii::$app->session->set('sd_banner_last_list', $bannerLastList);
+            }
             return Yii::$app->view->render(
                 '@app/views/parts/banner.twig',
                 [
