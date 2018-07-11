e1e675dd9458f4c62a0d4b9a3e42edbd827d76dd
Andrey <oxygenn@list.ru>
2018-07-11 10:11:58 +0300

    Скрулл после загрузки айаксом на до блока сортировки

--- source/js/original/new/ajax-load.js
+++ source/js/original/new/ajax-load.js
@@ -4,12 +4,20 @@
         e.preventDefault();
         var that = this;
         var url = $(that).attr('href');
+        var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+        var storesSort = $('.catalog-stores_sort');
+        var scrollTop = storesSort.length ? $(storesSort[0]).offset().top - $('#header>*').eq(0).height() - 50 : 0;
+
         $(that).addClass('loading');
         $.get(url, {'g':'ajax_load'}, function(data){
             var content = $(data).find('#content-wrap').html();
             $('body').find('#content-wrap').html(content);
             window.history.pushState("object or string", "Title", url);
-            $('html, body').animate({scrollTop: 0}, 500);
+
+            if (top > scrollTop) {
+                $('html, body').animate({scrollTop: scrollTop}, 500);
+            }
+
         }).fail(function() {
             $(that).removeClass('loading');
             notification.notifi({type:'err', 'title':lg('error'), 'message':lg('error_querying_data')});
