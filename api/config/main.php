0f002a7622e356ff25a43cc0a7b5453d387adb21
Max Matiukhin <matuhinmax@mail.ru>
2018-07-18 14:03:14 +0300

    Базовая настройка API и OAuth2 сервера

---
+++ api/config/main.php
@@ -0,0 +1,60 @@
+<?php
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php')/*,
+    require(__DIR__ . '/params-local.php')*/
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),    
+    'bootstrap' => ['log'],
+    'modules' => [
+        'oauth2' => [
+            'class' => 'filsh\yii2\oauth2server\Module',
+            'tokenParamName' => 'accessToken',
+            'tokenAccessLifetime' => 3600 * 24,
+            'storageMap' => [
+                'user_credentials' => 'app\models\User',
+            ],
+            'grantTypes' => [
+                'user_credentials' => [
+                    'class' => 'OAuth2\GrantType\UserCredentials',
+                ],
+                'refresh_token' => [
+                    'class' => 'OAuth2\GrantType\RefreshToken',
+                    'always_issue_new_refresh_token' => true
+                ]
+            ]
+        ],
+    ],
+    'components' => [        
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => false,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+
+            ],        
+        ]
+    ],
+    'params' => $params,
+];
+
+
+
