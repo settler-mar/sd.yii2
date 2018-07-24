d3a7405e88b37f76bad4c05f2f2f31e63dbffe5a
Andrey <oxygenn@list.ru>
2018-07-24 20:43:10 +0300

    API payments

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -11,6 +11,8 @@ use filsh\yii2\oauth2server\filters\auth\CompositeAuth;
 use yii\rest\ActiveController;
 use yii\web\Controller;
 use frontend\modules\stores\models\Stores;
+use frontend\modules\stores\models\CpaLink;
+use frontend\modules\payments\models\Payments;
 
 
 class SiteController extends Controller
@@ -18,6 +20,8 @@ class SiteController extends Controller
 
     protected $page;
     protected $onPage;
+    protected $dateFrom;
+
     /**
      * @inheritdoc
      */
@@ -36,7 +40,6 @@ class SiteController extends Controller
 //                'class' => ErrorToExceptionFilter::className()
 //            ],
         ]);
-
     }
 
     public function beforeAction($action)
@@ -72,12 +75,54 @@ class SiteController extends Controller
         return ['meta' => ['page' => $this->page, 'on-page' => $this->onPage, 'count' => $count], 'stores' => $stores];
     }
 
+    public function actionPayments()
+    {
+        $this->getRequest();
+        $payments = Payments::find()
+            ->from(Payments::tableName() . ' cwp')
+            ->select([
+                'cwcl.stores_id as store_id',
+                'cwp.action_id as action_id',
+                'order_id',
+                'click_date',
+                'action_date',
+                'status_updated',
+                'closing_date',
+                'order_price',
+                'cws.currency as store_currency',
+                'reward',
+                'cashback',
+                'concat("' . Yii::$app->user->identity->currency . '") as user_currency',
+                'kurs',
+                'user_id',
+                'sub_id',
+                'IF(status=2,"Confirmed",IF(status=1,"Canceled","Waiting")) as status',
+            ])
+            ->leftJoin(CpaLink::tableName() .' cwcl', 'cwp.cpa_id = cwcl.cpa_id and cwp.affiliate_id = cwcl.affiliate_id')
+            ->innerJoin(Stores::tableName() . ' cws', 'cws.uid = cwcl.stores_id')
+            ->where(['user_id'=> Yii::$app->user->id])
+            ->asArray();
+        if ($this->dateFrom) {
+            $payments->andWhere(['>=', 'click_date', $this->dateFrom]);
+        }
+        $count = $payments->count();
+        $payments = $payments->limit($this->onPage)
+            ->offset($this->onPage * ($this->page - 1))
+            ->all();
+        return [
+            'meta' => ['page' => $this->page, 'on-page' => $this->onPage, 'count' => $count, 'date-from'=>$this->dateFrom],
+            'payments' => $payments
+        ];
+    }
+
     protected function getRequest()
     {
         $request = Yii::$app->request;
         $this->page = $request->get('page') && (int)$request->get('page') > 0 ? (int)$request->get('page') : 1;
         $this->onPage = $request->get('on-page') && (int)$request->get('on-page') > 0 ?
             (int)$request->get('on-page') : 100;
+        $this->dateFrom = $request->get('date-from') && $request->get('date-from') != '' ?
+            $request->get('date-from') : false;
     }
 
 }
