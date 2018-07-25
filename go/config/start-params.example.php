c7a29743ae284f0f1950363edb50ddacaa15cba8
Andrey <oxygenn@list.ru>
2018-07-25 20:41:18 +0300

    Настройки для go

---
+++ go/config/start-params.example.php
@@ -0,0 +1,5 @@
+<?php
+
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_LOG_LEVEL') or define('YII_LOG_LEVEL', 0); //0- none  3-max
+defined('YII_ENV') or define('YII_ENV', 'prod'); // prod dev
