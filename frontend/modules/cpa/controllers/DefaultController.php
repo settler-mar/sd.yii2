7f0cec76b08fb15edf23bb286d2a7f87c128095f
Andrey <oxygenn@list.ru>
2018-07-26 23:36:59 +0300

    Callback с Awin начало

--- frontend/modules/cpa/controllers/DefaultController.php
+++ frontend/modules/cpa/controllers/DefaultController.php
@@ -96,4 +96,172 @@ class DefaultController extends Controller
             }
         }
     }
+
+    /**
+     * collback с Awin
+     */
+    public function actionAwin()
+    {
+        /*
+         array(17) {
+  ["transactionId"]=>
+  string(9) "105956531"
+  ["transactionDate"]=>
+  string(19) "2013-06-13 12:05:00"
+  ["transactionCurrency"]=>
+  string(3) "GBP"
+  ["transactionAmount"]=>
+  string(5) "26.09"
+  ["trackedCurrency"]=>
+  string(3) "EUR"
+  ["trackedAmount"]=>
+  string(5) "34.04"
+  ["affiliateId"]=>
+  string(6) "45628"
+  ["merchantId"]=>
+  string(4) "3661"
+  ["groupId"]=>
+  string(1) "0"
+  ["bannerId"]=>
+  string(1) "0"
+  ["clickRef"]=>
+  string(14) "LINKCMP2013-06"
+  ["clickRef4"]=>
+  string(12) "User82828211"
+  ["clickThroughTime"]=>
+  string(19) "2013-06-13 12:00:00"
+  ["commission"]=>
+  string(4) "1.64"
+  ["clickTime"]=>
+  string(19) "2013-06-13 12:00:00"
+  ["url"]=>
+  string(55) "http://www.publisher-domain.com/advertiser/product/"
+  ["phrase"]=>
+  string(16) "Electronic music"
+  ["searchEngine"]=>
+  string(15) "MSN Live Search"
+  ["commissionGroups"]=>
+  array(2) {
+    [0]=>
+    array(4) {
+      ["id"]=>
+      string(5) "47963"
+      ["name"]=>
+      string(2) "CD"
+      ["code"]=>
+      string(2) "CD"
+      ["description"]=>
+      string(2) "CD"
+    }
+    [1]=>
+    array(4) {
+      ["id"]=>
+      string(5) "47965"
+      ["name"]=>
+      string(3) "DVD"
+      ["code"]=>
+      string(3) "DVD"
+      ["description"]=>
+      string(3) "DVD"
+    }
+  }
+  ["products"]=>
+  array(2) {
+    [0]=>
+    array(7) {
+      ["productName"]=>
+      string(26) "The Knife – Silent Shout"
+      ["unitPrice"]=>
+      string(4) "5.55"
+      ["skuType"]=>
+      string(0) ""
+      ["skuCode"]=>
+      string(10) "B000EMSUQA"
+      ["quantity"]=>
+      string(1) "2"
+      ["category"]=>
+      string(16) "Electronic Music"
+      ["cgId"]=>
+      string(5) "47963"
+    }
+    [1]=>
+    array(7) {
+      ["productName"]=>
+      string(17) "Sigur Ros - Heima"
+      ["unitPrice"]=>
+      string(5) "14.99"
+      ["skuType"]=>
+      string(0) ""
+      ["skuCode"]=>
+      string(10) "B000EMSUQA"
+      ["quantity"]=>
+      string(1) "1"
+      ["category"]=>
+      string(9) "Music DVD"
+      ["cgId"]=>
+      string(5) "47965"
+    }
+  }*/
+        $cpa = Cpa::find()->where(['name' => 'Awin'])->one();
+        if (!$cpa) {
+            throw new \yii\web\NotFoundHttpException();
+            return false;
+        }
+
+        $request = Yii::$app->request;
+        $data = json_decode($request->post('AwinTransactionPush'), true);
+        if (!$data) {
+            throw new \yii\web\NotFoundHttpException();
+            return false;
+        }
+
+        $cpaLink = CpaLink::findOne(['cpa_id' => $cpa->id, 'affiliate_id' => $data['merchantId']]);//или affiliateId - пока непонятно
+        if (!$cpaLink) {
+            throw new \yii\web\NotFoundHttpException();
+            return false;
+        }
+
+        //$payment = Payments::findOne(['action_id'=> $data['transactionId'], 'affiliate_id' => $cpaLink->affiliate_id]);
+//        if ($payment) {
+//            Yii::$app->response->statusCode = 200;
+//            return json_encode(['status' => 'ok']);
+//        } else {
+        $date = date('Y-m-d H:i:s');
+
+        $payment = [
+//            'status' => 2,
+//            'subid' => $user->uid,
+//            'positions' => false,
+//            'action_id' => $data['transactionId'],
+//            'cart' => 0,
+//            'payment' => $comission ? $comission / 100 : 0,
+//            'click_date' =>  $date,
+//            'action_date' =>  $date,
+//            'status_updated' =>  $date,
+//            'closing_date' =>  $date,
+//            'product_country_code' => null,
+//            'order_id' => $paymentId,
+//            'tariff_id' => null,
+//            'currency' => 'EUR',
+//            'affiliate_id' => $cpaLink->affiliate_id,
+//            'cpa_id' => $cpa->id
+        ];
+        $paymentStatus = Payments::makeOrUpdate(
+            $payment,
+            $cpaLink->store,
+            $user,
+            null,
+            ['notify' => true, 'email' => true]
+        );
+        if ($paymentStatus['save_status']) {
+            Yii::$app->response->statusCode = 200;
+            return json_encode(['status' => 'ok']);
+        } else {
+            Yii::$app->response->statusCode = 500;
+            return json_encode(['status' => 'error']);
+        }
+        //}
+
+
+    }
 }
