2feeb2b35b3a13255622947c71ce6797a91fbcc8
Andrey <oxygenn@list.ru>
2018-08-29 11:19:54 +0300

    Rakuten шопы через addOrUpdate, поправил купоны

--- console/controllers/RakuteController.php
+++ console/controllers/RakuteController.php
@@ -25,6 +25,8 @@ class RakuteController extends Controller
     private $cpa_id;
     private $siteId;
     private $cpaLinkInserted;
+    private $fails;
+    private $failsCpaLink;
 
   public function init()
   {
@@ -147,139 +149,38 @@ class RakuteController extends Controller
             $linkurl ="https://click.linksynergy.com/deeplink?id=".$this->siteId."&mid=".$merchant['mid']."&u1={{subid}}&murl=".$siteUrl;
         } else {
             $linkurl = '-';
+            $siteUrl = '-';
         }
-        $route = Yii::$app->help->str2url($merchant['merchantname']);
-
         $logoFile = 'http://merchant.linksynergy.com/fs/logo/link_'.$merchant['mid'].'.jpg';
-        $logo = explode(".", $logoFile);
-        $logo = 'cw2_' . $route . '.' . $logo[count($logo) - 1];
-        $logo = str_replace('_', '-', $logo);
-
-
         $affiliate_id = $merchant['mid'];
         $this->affiliate_list[] = $affiliate_id;
-        $merchant['currency'] = 'USD';
-        $merchant['name'] = $merchant['merchantname'];
-        //$status = $merchant['applicationStatus'] == 'Approved' ? 1 : 0;
-        $status = 1;
 
-        $cpa_link = CpaLink::findOne(['cpa_id' => $this->cpa_id, 'affiliate_id' => $affiliate_id]);
-
-        $cpa_id = false;
-
-        if ($cpa_link) {
-            //если CPA link нашли то проверяем ссылку и при необходимости обновляем ее
-            if ($cpa_link->affiliate_link != $linkurl) {
-                $cpa_link->affiliate_link = $linkurl;
-                $cpa_link->save();
-            }
-
-            $cpa_id = $cpa_link->id;
-
-            //переходим от ссылки СПА на магазин
-            $db_store = $cpa_link->store;
-                if ($db_store && ($db_store->logo == $logo || !$db_store->logo)) {
-                    $test_logo = true;
-                } else {
-                    $test_logo = false;
-                }
-        } else {
-            $db_store = false;
-                    $test_logo = true;
+        $store = [
+            'logo' => $logoFile,
+            'cpa_id' => $this->cpa_id,
+            'affiliate_id' => $affiliate_id,
+            'url' => $siteUrl,
+            'name' => $merchant['merchantname'],
+            'currency' => 'USD',
+            'cashback' => '',
+            'hold_time' => 30,
+            'status' => 1,
+            'affiliate_link' => $linkurl,
+        ];
+        //d($store);
+        $result = Stores::addOrUpdate($store);
+        if (!$result['result']) {
+            $this->fails++;
         }
-
-        $is_new = false; //метка если более выский уровень вновь созданный
-
-
-        //если лого то проверяем его наличие и при нобходимости обновляем
-        if ($test_logo) {
-            //проверяем лого на папки
-            $path = Yii::$app->getBasePath() . '/../frontend/web/images/logos/';
-            if (!file_exists($path)) {
-                mkdir($path, 0777, true);
-            }
-            //проверяем лого на наличие
-            if (!file_exists($path . $logo)) {
-                $file = file_get_contents($logoFile);
-                $image = new Image($file);
-                $image->bestFit(192, 192);
-                $image->saveAs($path . $logo);
-                //file_put_contents($path . $logo, $file);
-            }
+        if ($result['new']) {
+            $this->inserted++;
         }
-
-        //если магазин не нашли по прямому подключению пробуем найти по косвеным признакам
-
-        //поиск по ссылке на роуту
-        if (!$db_store) {
-            $db_store = Stores::find()->where(['route' => $route])->one();
-        }
-
-        //Если магазин так и не нашли то создаем
-        if (!$db_store) {
-            $db_store = new Stores();
-            $db_store->name = $merchant['merchantname'];
-            $db_store->route = $route;
-            $db_store->is_active = $status;
-            $db_store->url = '';
-            $db_store->logo = $logo;
-            $db_store->currency = 'USD';//$store['currency'];
-            $db_store->hold_time = 30;//$store['max_hold_time'] ? (int)$store['max_hold_time'] : 30;
-            $db_store->percent = 50;
-           // $db_store->displayed_cashback = $cashbackClean;
-            if (!$db_store->save()) {
-                d($db_store->errors);
-            } else {
-                $this->inserted++;
-            }
-        } elseif ($test_logo && !empty($logo)) {
-                //если нашли, но лого нужно обновить, то обновляем
-            $db_store->logo = $logo;
-        }
-
-        $store_id = $db_store->uid;
-
-        //если нет в базе CPA ЛИНК то создаем ее
-        if (!$cpa_id) {
-            echo "new cpalink\n";
-            $cpa_link = new CpaLink();
-            $cpa_link->cpa_id = $this->cpa_id;
-            $cpa_link->stores_id = $store_id;
-            $cpa_link->affiliate_id = $affiliate_id;
-            $cpa_link->affiliate_link = $linkurl;
-            if (!$cpa_link->save()) {
-                d($cpa_link->errors);
-                return;
-            }
+        if ($result['newCpa']) {
             $this->cpaLinkInserted++;
-
-            $cpa_id = $cpa_link->id;
-            $is_new = true;
-        } else {
-            //проверяем свяль CPA линк и магазина
-            if ($cpa_link->stores_id != $store_id) {
-                $cpa_link->stores_id = $store_id;
-                $cpa_link->save();
+            if (!$result['resultCpa']) {
+                $this->failsCpaLink++;
             }
-            $is_new = false;
         }
-
-        //если СPA не выбранна то выставляем текущую
-        //echo $db_store->active_cpa.' '.$cpa_id."\n";
-        if ((int)$db_store->active_cpa == 0 || empty($db_store->active_cpa)) {
-            $db_store->active_cpa = $cpa_id;
-        }
-
-        //$db_store->displayed_cashback = $cashbackClean;
-
-        //$db_store->url = '';
-
-        if ($db_store->is_active != -1) {
-            $db_store->is_active = 1;
-        }
-        $db_store->save();
-
-
     }
 
     /** запись купона
@@ -287,8 +188,8 @@ class RakuteController extends Controller
      */
     private function writeCoupon($coupon)
     {
-        $route = Yii::$app->help->str2url($coupon['advertisername']);
-        $store = $this->getStore($route);
+        //d($coupon);
+        $store = $this->getStore($coupon['advertiserid']);
         if (!$store) {
             echo "Store ".$coupon['advertisername']." not found\n";
             return;
@@ -379,12 +280,13 @@ class RakuteController extends Controller
         }
     }
 
-    private function getStore($route)
+    private function getStore($affiliateId)
     {
-        if (!isset($this->stores[$route])) {
-            $this->stores[$route] = Stores::find()->where(['route' => $route])->one();
+        if (!isset($this->stores[$affiliateId])) {
+            $cpaLink = CpaLink::findOne(['cpa_id' => $this->cpa_id, 'affiliate_id' => $affiliateId]);
+            $this->stores[$affiliateId] = $cpaLink ? $cpaLink->store : false;
         }
-        return $this->stores[$route];
+        return $this->stores[$affiliateId];
     }
 
 }
