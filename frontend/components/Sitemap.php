75592a6f5d7dd685109860dcae28b1fa671f32b7
Andrey <oxygenn@list.ru>
2018-10-29 20:28:01 +0300

    Шопы и категории online-offline

--- frontend/components/Sitemap.php
+++ frontend/components/Sitemap.php
@@ -18,20 +18,58 @@ class Sitemap
             ],
             'friquency' => 'monthly'
         ],
-        [
+        [   //категории шопов
             'model' => 'frontend\modules\stores\models\CategoriesStores',
             'priority' => 1,
-            'condition' => ['is_active' => 1],
+            'join' => [
+                ['cw_stores_to_categories cwstc', 'cwstc.category_id = cw_categories_stores.uid'],
+                ['cw_stores cws', 'cws.uid = cwstc.store_id']
+            ],
+            'condition' => [
+                'and',
+                ['cw_categories_stores.is_active' => 1],
+                ['cws.is_active' => [0,1]],
+                ['cws.is_offline' => 0],
+                ['is not', 'cws.uid', null]
+            ],
+            'select' => ['cw_categories_stores.route', 'cw_categories_stores.updated_at'],
+            'url' => '/stores/{{route}}',
+            'friquency' => 'daily'
+        ],
+        [   //категории шопов offline
+            'model' => 'frontend\modules\stores\models\CategoriesStores',
+            'join' => [
+                ['cw_stores_to_categories cwstc', 'cwstc.category_id = cw_categories_stores.uid'],
+                ['cw_stores cws', 'cws.uid = cwstc.store_id']
+            ],
+            'priority' => 1,
+            'condition' => [
+                'and',
+                ['cw_categories_stores.is_active' => 1],
+                ['cws.is_active' => [0,1]],
+                ['cws.is_offline' => 1],
+                ['is not', 'cws.uid', null]
+            ],
+            'select' => ['cw_categories_stores.route', 'cw_categories_stores.updated_at'],
+            'url' => '/stores/{{route}}-offline',
+            'friquency' => 'daily'
+        ],
+        [
+            //шопы
+            'model' => 'frontend\modules\stores\models\Stores',
+            'priority' => 1,
+            'condition' => ['and', ['is_active' => [0, 1]], ['is_offline' => 0]],
             'select' => ['route', 'updated_at'],
             'url' => '/stores/{{route}}',
             'friquency' => 'daily'
         ],
         [
+            //шопы offline
             'model' => 'frontend\modules\stores\models\Stores',
             'priority' => 1,
-            'condition' => ['is_active' => [0, 1]],
+            'condition' => ['and', ['is_active' => [0, 1]], ['is_offline' => 1]],
             'select' => ['route', 'updated_at'],
-            'url' => '/stores/{{route}}',
+            'url' => '/stores/{{route}}-offline',
             'friquency' => 'daily'
         ],
         [
@@ -41,29 +79,29 @@ class Sitemap
             'url' => '/coupons/{{route}}',
             'friquency' => 'daily'
         ],
-        [
-            //купоны шопа - только для шопов, имеющих купоны, 2 url
-            'model' => 'frontend\modules\stores\models\Stores',
-            'priority' => 1,
-            'join' => ['cw_coupons cwc', 'cwc.store_id = cw_stores.uid'],
-            'condition' => ['and', ['cw_stores.is_active' => [0, 1]], ['is not', 'cwc.uid', null]],
-            'select' => ['route', 'cw_stores.updated_at'],
-            'url' => [
-                ['url' => '/coupons/{{route}}', 'priority' => 1, 'friquency' => 'daily'],
-                ['url' => '/coupons/{{route}}/expired', 'priority' => 1, 'friquency' => 'daily']
-            ],
-            'friquency' => 'daily'
-        ],
-        [
-            //купоны пока ограничить по времени
-            'model' => 'frontend\modules\coupons\models\Coupons',
-            'priority' => 1,
-            'join' => ['cw_stores cws', 'cws.uid = cw_coupons.store_id'],
-            'condition' => ['and',['cws.is_active' => [0, 1]], ['>', 'cw_coupons.date_end', '2018-10-01']],
-            'select' => ['cws.route as route', 'cw_coupons.uid', 'cw_coupons.updated_at'],
-            'url' => '/coupons/{{route}}/{{uid}}',
-            'friquency' => 'daily'
-        ],
+//        [
+//            //купоны шопа - 2 url
+//            'model' => 'frontend\modules\stores\models\Stores',
+//            'priority' => 1,
+//            'join' => [['cw_coupons cwc', 'cwc.store_id = cw_stores.uid']],
+//            'condition' => ['and', ['cw_stores.is_active' => [0, 1]], ['is not', 'cwc.uid', null]],
+//            'select' => ['route', 'cw_stores.updated_at'],
+//            'url' => [
+//                ['url' => '/coupons/{{route}}', 'priority' => 1, 'friquency' => 'daily'],
+//                ['url' => '/coupons/{{route}}/expired', 'priority' => 1, 'friquency' => 'daily']
+//            ],
+//            'friquency' => 'daily'
+//        ],
+//        [
+//            //купоны пока ограничить по времени
+//            'model' => 'frontend\modules\coupons\models\Coupons',
+//            'priority' => 1,
+//            'join' => [['cw_stores cws', 'cws.uid = cw_coupons.store_id']],
+//            'condition' => ['and',['cws.is_active' => [0, 1]], ['>', 'cw_coupons.date_end', '2018-10-01']],
+//            'select' => ['cws.route as route', 'cw_coupons.uid', 'cw_coupons.updated_at'],
+//            'url' => '/coupons/{{route}}/{{uid}}',
+//            'friquency' => 'daily'
+//        ],
         [
             //единичный юрл
             'url' => '/reviews',
@@ -71,8 +109,6 @@ class Sitemap
             'friquency' => 'daily',
             'priority' => 1,
         ]
-
-
     ];
     protected $languages = [];
     protected $url;
@@ -108,7 +144,9 @@ class Sitemap
                         $model->where($mapItem['condition']);
                     }
                     if (!empty($mapItem['join'])) {
-                        $model->leftJoin($mapItem['join'][0], $mapItem['join'][1]);
+                        foreach ($mapItem['join'] as $join) {
+                            $model->leftJoin($join[0], $join[1]);
+                        }
                     }
                     if (!empty($mapItem['select'])) {
                         $model->select($mapItem['select']);
