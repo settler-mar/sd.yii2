e9b1bb235586bd5de08e5cdd29132fb49819899c
Max Matiukhin <matuhinmax@mail.ru>
2018-12-05 08:42:15 +0200

    Мелкие правки

--- common/config/main.php
+++ common/config/main.php
@@ -208,7 +208,10 @@ if (YII_DEBUG) {
   $config['bootstrap'][] = 'debug';
   $config['modules']['debug'] = [
       'class' => 'yii\debug\Module',
-      'allowedIPs' => ['*']
+      'allowedIPs' => ['*'],
+      'panels' => [
+          'queue' => \yii\queue\debug\Panel::className(),
+      ],
   ];
   $config['bootstrap'][] = 'gii';
   $config['modules']['gii'] = [
