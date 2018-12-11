4165ee1dcdf6e491d75b71bcac5ae29ff0534e80
Max Matiukhin <matuhinmax@mail.ru>
2018-12-11 10:31:24 +0200

    переделал работу с FTP через curl

--- common/models/Impact.php
+++ common/models/Impact.php
@@ -6,86 +6,100 @@ use yii;
 
 class Impact
 {
-    private $config;
+  private $config;
 
-    public function __construct()
-    {
-        if (isset(Yii::$app->params['impact'])) {
-            $this->config = Yii::$app->params['impact'];
-        } else {
-            ddd('Нет конфиг impact');
-        }
+  public function __construct()
+  {
+    if (isset(Yii::$app->params['impact'])) {
+      $this->config = Yii::$app->params['impact'];
+    } else {
+      ddd('Нет конфиг impact');
+    }
+  }
+
+  /**
+   * список файлов каталога
+   * @return \SimpleXMLElement
+   */
+  public function getCatalogList()
+  {
+    $file = $this->getFtpFile('/catalogs_info_file.xml');
+    if ($file) {
+      $content = file_get_contents($file);
+      $xml = simplexml_load_string($content);
+      return $xml;
+    }
+  }
+
+  /**
+   * получение каталога
+   * @param $file
+   * @param bool $refresh
+   * @return string
+   */
+  public function getCatalog($file, $refresh = true)
+  {
+    $archive = $this->getFtpFile($file, $refresh);
+    $data = gzfile($archive);
+    $archiveArray = explode('.', $archive);
+    $newName = implode('.', array_splice($archiveArray, 0, count($archiveArray) - 1));
+    file_put_contents($newName, $data);
+    return $newName;
+  }
+
+  /**
+   * удаление архива и файла каталога
+   * @param $file
+   */
+  public function unlink($file)
+  {
+    $fileNameArray = array_diff(explode('/', $file), ['']);
+    $localName = Yii::getAlias('@runtime/' . implode('_', $fileNameArray));
+    unlink($localName);
+    $archiveArray = explode('.', $localName);
+    $newName = implode('.', array_splice($archiveArray, 0, count($archiveArray) - 1));
+    unlink($newName);
+  }
+
+
+  /**
+   * @param $file
+   * @return bool|string
+   */
+  private function getFtpFile($file, $refresh = true)
+  {
+    $fileNameArray = array_diff(explode('/', $file), ['']);
+    $localName = Yii::getAlias('@runtime/' . implode('_', $fileNameArray));
+    if (!$refresh && file_exists($localName)) {
+      return $localName;
     }
 
-    /**
-     * список файлов каталога
-     * @return \SimpleXMLElement
-     */
-    public function getCatalogList()
-    {
-        $file = $this->getFtpFile('catalogs_info_file.xml');
-        if ($file) {
-            $content = file_get_contents($file);
-            $xml = simplexml_load_string($content);
-            return $xml;
-        }
+    $file = 'ftp://' .
+        //$this->config['ftp_login'] . ':' . $this->config['ftp_password'].'@'.
+        trim($this->config['ftp_server'], '/') . '/' . trim($file, '/');
+
+
+    $curl = curl_init();
+    $file_local = fopen($localName, 'w');
+
+    curl_setopt($curl, CURLOPT_URL, $file); #input
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_FILE, $file_local); #output
+    curl_setopt($curl, CURLOPT_USERPWD, $this->config['ftp_login'] . ':' . $this->config['ftp_password']);
+
+    if (curl_exec($curl) === false) {
+      ddd('Ошибка curl: ' . curl_error($curl) . "\n$localName\n$file\n");
     }
 
-    /**
-     * получение каталога
-     * @param $file
-     * @param bool $refresh
-     * @return string
-     */
-    public function getCatalog($file, $refresh = true)
-    {
-        $archive = $this->getFtpFile($file, $refresh);
-        $data = gzfile($archive);
-        $archiveArray = explode('.', $archive);
-        $newName = implode('.', array_splice($archiveArray, 0, count($archiveArray) -1));
-        file_put_contents($newName, $data);
-        return $newName;
-    }
+    curl_close($curl);
+    fclose($file_local);
 
-    /**
-     * удаление архива и файла каталога
-     * @param $file
-     */
-    public function unlink($file)
-    {
-        $fileNameArray = array_diff(explode('/', $file), ['']);
-        $localName = Yii::getAlias('@runtime/' . implode('_', $fileNameArray));
-        unlink($localName);
-        $archiveArray = explode('.', $localName);
-        $newName = implode('.', array_splice($archiveArray, 0, count($archiveArray) -1));
-        unlink($newName);
+    if (!is_readable($localName)) {
+      ddd('Фаил не сохранился.' . "\n$localName\n$file\n");
     }
 
 
-    /**
-     * @param $file
-     * @return bool|string
-     */
-    private function getFtpFile($file, $refresh = true)
-    {
-        $fileNameArray = array_diff(explode('/', $file), ['']);
-        $localName = Yii::getAlias('@runtime/' . implode('_', $fileNameArray));
-        if (!$refresh && file_exists($localName)) {
-            return $localName;
-        }
 
-        $connection = ftp_connect($this->config['ftp_server']);
-        $login_result = ftp_login($connection, $this->config['ftp_login'], $this->config['ftp_password']);
-        if (ftp_get($connection, $localName, $file, FTP_BINARY)) {
-            d('downoaded ' . $file);
-        } else {
-            d("Ошибка");
-            return false;
-        }
-
-        ftp_close($connection);
-        return $localName;
-    }
-
-
+    return $localName;
+  }
 }
