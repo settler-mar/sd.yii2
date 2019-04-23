8ccc053d5c397a7d3eaa64d4199ce229a97d067a
Max Matiukhin <matuhinmax@mail.ru>
2019-04-23 13:38:49 +0300

    API для APP

--- common/models/Cj.php
+++ common/models/Cj.php
@@ -1,78 +1,114 @@
 <?php
-
+//Ключ авторизации
+//https://developers.cj.com/authentication/overview
 namespace common\models;
 
 use yii;
+use rdx\graphqlquery\Query as gql;
 
 class Cj
 {
-    private $devKey;
-    private $siteId;
-    private $url = 'https://advertiser-lookup.api.cj.com/v3/advertiser-lookup';
-    private $commissionUrl = 'https://commission-detail.api.cj.com/v3/commissions';
-    private $urlLinkSearch = 'https://linksearch.api.cj.com/v2/link-search';
+  private $devKey;
+  private $siteId;
+  private $url = 'https://advertiser-lookup.api.cj.com/v2/advertiser-lookup';
+  private $commissionUrl = 'https://commission-detail.api.cj.com/v3/commissions';
+  private $urlLinkSearch = 'https://link-search.api.cj.com/v2/link-search';
 
 
-    public function __construct()
-    {
-        $config = Yii::$app->params['cj.com'];
-        $this->devKey = $config && isset($config['dev_key']) ? $config['dev_key'] : '';
-        $this->siteId = $config && isset($config['site_id']) ? $config['site_id'] : '';
+  public function __construct()
+  {
+    $config = Yii::$app->params['cj.com'];
+    $this->devKey = $config && isset($config['dev_key']) ? $config['dev_key'] : '';
+    $this->siteId = $config && isset($config['site_id']) ? $config['site_id'] : '';
+  }
+
+  public function getJoined($page, $perPage)
+  {
+    return $this->getRequest($this->url, [
+        'advertiser-ids' => 'joined',
+        'page-number' => $page,
+        'records-per-page' => $perPage,
+    ]);
+  }
+
+  public function getLinks($page, $perPage, $options = [])
+  {
+    $data = $this->getRequest($this->urlLinkSearch, array_merge([
+        'website-id' => $this->siteId,
+        'advertiser-ids' => 'joined',
+        'page-number' => $page,
+        'records-per-page' => $perPage,
+      //'link-type' => 'Text Link'
+    ], $options));
+    return $data;
+  }
+
+  public function getPayments($dateStart = false, $dateEnd = false)
+  {
+    $dateEnd = $dateEnd ? $dateEnd : time();
+    $dateStart = $dateStart ? $dateStart : $dateEnd - 3600 * 24 * 30;
+
+    $data = "{ publisherCommissions(".
+      "forPublishers: [\"4701066\"],".
+      //"forAdvertisers: [\"4701066\"],".
+      "sincePostingDate:\"".date('Y-m-d',$dateStart)."T23:59:59Z\",".
+      "beforePostingDate:\"".date('Y-m-d',$dateEnd)."T23:59:59Z\")".
+      "{count payloadComplete records {actionTrackerName websiteName advertiserName postingDate pubCommissionAmountUsd items { quantity perItemSaleAmountPubCurrency totalCommissionPubCurrency }  }  } }";
+    $data = $this->getRequest("https://commissions.api.cj.com/query", $data,true,false);
+
+    ddd($data);
+  }
+
+
+  private function GraphQLRequest($name,$params){
+    $query = gql::query($name);
+    $query->defineFragment('userStuff', 'User');
+    //$query->viewer->fields('...userStuff', 'repos');
+    //$query->viewer->attribute('public', true);
+    //ddd($query->viewer);
+    return "{ advertiserCommissions(forPublishers: [\"4701066\"], sincePostingDate:\"2018-08-08T00:00:00Z\",beforePostingDate:\"2018-08-09T00:00:00Z\"){count payloadComplete records {actionTrackerName websiteName advertiserName postingDate pubCommissionAmountUsd items { quantity perItemSaleAmountPubCurrency totalCommissionPubCurrency }  }  } }";
+    return $query->build();
+  }
+
+  private function getRequest($url, $params,$post = false,$isXml = true)
+  {
+    if(is_array($params)){
+      $query = http_build_query($params);
+    }else{
+      $query = $params;
     }
 
-    public function getJoined($page, $perPage)
-    {
-        return $this->getRequest($this->url, [
-            'advertiser-ids'=>'joined',
-            'page-number' => $page,
-            'records-per-page' => $perPage,
-        ]);
+    $url = $url . (!$post && $query ? '?' . $query : '');
+
+    d($url,$query);
+
+    $headers = ["Authorization: " . $this->devKey];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    if($post){
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS,$query);
     }
 
-    public function getLinks($page, $perPage, $options = [])
-    {
-        return $this->getRequest($this->urlLinkSearch, array_merge([
-            'website-id' => $this->siteId,
-            'advertiser-ids' => 'joined',
-            'page-number' => $page,
-            'records-per-page' => $perPage,
-            //'link-type' => 'Text Link'
-        ], $options));
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    if($isXml){
+      $data =  json_encode((array)simplexml_load_string($response));
+    }else{
+      $data = $response;
     }
 
-    public function getPayments($dateStart = false, $dateEnd = false)
-    {
-        $dateEnd = $dateEnd ? $dateEnd : time();
-        $dateStart = $dateStart ? $dateStart : $dateEnd - 3600 * 24 * 30;
-        return $this->getRequest($this->commissionUrl, [
-            'date-type'=>'event', //'posting'
-            'end-date' => date('Y-m-d H:i:s', $dateEnd),
-            'start-date' => date('Y-m-d H:i:s', $dateStart),
-            //'action-types' => 'sale',
-
-        ]);
+    $data = json_decode($data, true);
+    if (isset($data['error-message'])) {
+      d($params);
+      echo 'Error: ' . $data['error-message'] . "\n";
+      return;
     }
-
-
-    private function getRequest($url, $params)
-    {
-        $query = http_build_query($params);
-        $url = $url . ($query ? '?'.$query : '');
-
-        $headers = ["authorization:". $this->devKey];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $data =  json_decode(json_encode((array)simplexml_load_string($response)), true);
-        if (isset($data['error-message'])) {
-            echo 'Error: '.$data['error-message']."\n";
-            return;
-        }
-        return $data;
-    }
+    return $data;
+  }
 
 }
